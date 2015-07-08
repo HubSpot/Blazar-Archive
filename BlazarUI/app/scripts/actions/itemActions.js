2fce68b0998f5c01831564e8a3bd827957d36fc5
Ben Lodge <blodge@hubspot.com>
2015-07-08 18:41:33 -0400

    create dashboard, sidebar, and initial flux stuff

---
+++ BlazarUI/app/scripts/actions/itemActions.js
@@ -0,0 +1,21 @@
+import Reflux from 'reflux';
+
+var RequestActions = Reflux.createActions([
+  'loadRequests',
+  'loadRequestsSuccess',
+  'loadRequestsError'
+]);
+
+RequestActions.loadRequests.preEmit = function(data){
+  // we will put api call/ async stuff here
+  // temporarily using setTimeout for faking async behaviour
+  setTimeout(function(){
+    var Requests = ['Australia', 'New Zealand', 'Singapore', 'Tonga'];
+    RequestActions.loadRequestsSuccess(Requests);
+
+    // on error
+    // RequestActions.loadRequestsError('an error occured');
+  },500);
+};
+
+export default RequestActions;
---
+++ BlazarUI/app/scripts/components/dashboard/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Dashboard extends React.Component{
+
+  constructor(props, context) {
+   super(props);
+  }
+
+  render() {
+    return (  
+      <div className='primary-content'>
+        Dashboard content
+      </div>
+    );
+  }
+
+}
+
+
+export default Dashboard;
