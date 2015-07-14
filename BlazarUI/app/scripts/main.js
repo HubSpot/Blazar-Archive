3e8af2829b2cdb097f5fc3298c8cf17a71e12fef
Ben Lodge <blodge@hubspot.com>
2015-07-14 08:18:59 -0400

    add push state for routes

--- BlazarUI/app/scripts/main.js
+++ BlazarUI/app/scripts/main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
 
-Router.run(routes, Handler => React.render(<Handler />, document.body));
+Router.run(routes, Router.HistoryLocation, Handler => React.render(<Handler />, document.body));
