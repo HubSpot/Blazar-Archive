15fa26d20181dbb127353ab4fdf78a4c1a02dc84
Jonathan Haber <jhaber@hubspot.com>
2015-07-15 14:59:22 -0400

    Ignore unknown config properties

--- BlazarService/src/main/java/com/hubspot/blazar/BlazarConfiguration.java
+++ BlazarService/src/main/java/com/hubspot/blazar/BlazarConfiguration.java
@@ -1,6 +1,7 @@
 package com.hubspot.blazar;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.dropwizard.Configuration;
 
-public class BlazarConfiguration extends Configuration {
-}
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BlazarConfiguration extends Configuration {}
