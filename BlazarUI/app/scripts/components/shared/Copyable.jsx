5078a5bd5486a6ecf18caefdcb7de2f307ed5b26
Ben Lodge <blodge@hubspot.com>
2015-07-15 17:13:21 -0400

    add clickable icons for git sha

---
+++ BlazarUI/app/scripts/components/shared/Copyable.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactZeroClipboard from 'react-zeroclipboard';
+import { bindAll } from 'underscore';
+
+class Copyable extends React.Component {
+
+  constructor(props) {
+    bindAll(this, 'handleClick', 'handleHover');
+  }
+
+  handleClick() {
+    this.props.click();
+  }
+
+  handleHover() {
+    console.log('hovering');
+    this.props.hover();
+  }
+
+  render() {
+    return (
+      <ReactZeroClipboard text={this.props.text}>
+        <span onClick={this.handleClick} onMouseOver={this.handleHover}>{this.props.children}</span>
+      </ReactZeroClipboard>
+    );
+  }
+
+}
+
+Copyable.propTypes = {
+  children: React.PropTypes.node.isRequired,
+  text: React.PropTypes.string.isRequired,
+  click: React.PropTypes.func,
+  hover: React.PropTypes.func
+};
+
+export default Copyable;
