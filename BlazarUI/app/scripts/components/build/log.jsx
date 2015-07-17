2f9ea76418b459a1b3d91682a92f7837b0675414
Ben Lodge <blodge@hubspot.com>
2015-07-17 18:40:21 -0400

    remove temporary markup

--- BlazarUI/app/scripts/components/build/log.jsx
+++ BlazarUI/app/scripts/components/build/log.jsx
@@ -16,17 +16,10 @@ Line.propTypes = {
 class Log extends React.Component {
 
   render() {
-    // simulate some lines
-    let lines = [];
-    for (let i = 0; i < 200; i++) {
-      lines.push(<Line key={i}>npm@1.4.28 /usr/share/hubspot/jenkins-home/workspace/ben-test/nvm/v0.10.32/lib/node_modules/npm</Line>);
-    }
 
     return (
       <pre className='log'>
-        <Line>npm http 200 https://registry.npmjs.org/npm/-/npm-1.4.28.tgz</Line>
-        <Line>/usr/share/hubspot/jenkins-home/workspace/ben-test/nvm/v0.10.32/bin/npm -> /usr/share/hubspot/jenkins-home/workspace/ben-test/nvm/v0.10.32/lib/node_modules/npm/bin/npm-cli.js</Line>
-        {lines}
+        <Line>No log</Line>
       </pre>
     );
   }
