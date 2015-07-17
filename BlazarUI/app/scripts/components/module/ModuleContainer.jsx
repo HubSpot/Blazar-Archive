fe229aec48db96a8eddde18030d290a0094c48b8
Ben Lodge <blodge@hubspot.com>
2015-07-17 14:06:10 -0400

    pass loading state to module component

--- BlazarUI/app/scripts/components/module/ModuleContainer.jsx
+++ BlazarUI/app/scripts/components/module/ModuleContainer.jsx
@@ -4,11 +4,18 @@ import PageContainer from '../layout/PageContainer.jsx';
 
 class ModuleContainer extends React.Component {
 
+  constructor() {
+    this.state = {
+      loading: true
+    };
+  }
+
   render() {
     return (
       <PageContainer>
         <Module
           params={this.props.params}
+          loading={this.state.loading}
         />
       </PageContainer>
     );
