d81d2359da5f370789d650d99cd8414f82671863
Ben Lodge <blodge@hubspot.com>
2015-07-17 20:14:48 -0400

    update colors and icons

--- BlazarUI/app/scripts/components/constants.js
+++ BlazarUI/app/scripts/components/constants.js
@@ -1,7 +1,16 @@
 export const labels = {
   'SUCCEEDED': 'success',
-  'FAILED': 'danger'
+  'FAILED': 'danger',
+  'CANCELLED': 'default',
+  'IN_PROGRESS': 'info'
 };
+
+export const iconStatus = {
+  'SUCCEEDED': 'check',
+  'FAILED': 'close',
+  'IN_PROGRESS': 'ship',
+  'CANCELLED': 'stop opacity-semi'
+}
 
 export const unicode = {
   'left-bracket': '\u007B',
