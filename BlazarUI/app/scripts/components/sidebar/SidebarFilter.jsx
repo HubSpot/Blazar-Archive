d662bd3e5a918b02ae40a812df6e9b55d76c1a08
Ben Lodge <blodge@hubspot.com>
2015-07-20 19:18:35 -0400

    work on filtering of builds list

--- BlazarUI/app/scripts/components/sidebar/SidebarFilter.jsx
+++ BlazarUI/app/scripts/components/sidebar/SidebarFilter.jsx
@@ -1,59 +1,121 @@
 import React from 'react';
-import Select from 'react-select';
-import ComponentHelpers from '../ComponentHelpers';
+import Typeahead from 'react-typeahead-component';
+import {bindAll} from 'underscore';
+// import SidebarFilterOption from './SidebarFilterOption.jsx';
+
+
+
+class SidebarFilterOption extends React.Component {
+
+  render() {
+    return (
+      <div>
+        {this.renderOption()}
+      </div>
+    );
+  }
+
+  renderOption() {
+    let optionData = this.props.data.module;
+    let inputValue = this.props.userInputValue;
+
+    if (optionData.indexOf(inputValue) === 0) {
+      return (
+        <span>
+          {inputValue}
+          <strong>
+            {optionData.slice(inputValue.length)}
+            </strong>
+        </span>
+      );
+    }
+    return optionData;
+  }
+
+}
+
+SidebarFilterOption.propTypes = {
+  data: React.PropTypes.any,
+  inputValue: React.PropTypes.string,
+  isSelected: React.PropTypes.bool,
+  userInputValue: React.PropTypes.string
+};
+
+
+
+
 
 class SidebarFilter extends React.Component {
 
   constructor(props) {
     super(props);
-    ComponentHelpers.bindAll(this, ['getOptions']);
+    this.state = {
+      inputValue: ''
+    };
+    bindAll(this, 'handleOptionClick', 'handleChange', 'handleFocus', 'handleBlur', 'onKeyDown');
   }
 
-  // user has clicked an options
-  selectionMade(val) {
-    console.log('user has clicked an option: ', val);
+  setInputValue(value) {
+    this.props.updateResults(value);
   }
 
-  // check if we need to expand any repos
-  checkFilterState(input, cb) {
-    this.props.updateResults(input);
-    cb();
+  // to do: use react link so we dont have
+  // a full page refresh
+  linkToBuild(link) {
+    window.location = link;
   }
 
-  // a slight hack to get the value the user typed,
-  // (as react-select doesnt have a direct keyup event)
-  // then we pass back the original options from props
-  getOptions(input, cb) {
-    let options = { options: this.props.modules };
-    this.checkFilterState(input, () => {
-      cb(null, options);
-    });
+  handleChange(event) {
+    this.setInputValue(event.target.value);
+  }
+
+  onKeyDown(event, build) {
+    if (event.keyCode === 13) {
+      this.linkToBuild(build.link);
+    }
+  }
+
+  handleOptionClick(event, build) {
+    this.linkToBuild(build.link);
+  }
+
+  handleFocus() {
+    this.props.filterInputFocus(true);
+  }
+
+  handleBlur() {
+    this.props.filterInputFocus(false);
   }
 
   render() {
-
     if (this.props.loading) {
       return <div></div>;
     }
+
     return (
-      <div className="form-group">
-        <Select
-          name="repolist"
-          value="Filter Modules..."
-          asyncOptions={this.getOptions}
-          onChange={this.selectionMade}
-          onBlur={this.blur}
-          autoload={false}
-        />
-      </div>
+      <Typeahead
+        ref='typeahead'
+        placeholder='Filter modules...'
+        inputValue={this.props.filterText}
+        options={this.props.modules}
+        onChange={this.handleChange}
+        optionTemplate={SidebarFilterOption}
+        onOptionClick={this.handleOptionClick}
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}
+        onKeyDown={this.onKeyDown}
+      />
     );
+
   }
 }
 
 SidebarFilter.propTypes = {
   updateResults: React.PropTypes.func.isRequired,
   modules: React.PropTypes.array.isRequired,
-  loading: React.PropTypes.bool.isRequired
+  loading: React.PropTypes.bool.isRequired,
+  filterText: React.PropTypes.string.isRequired,
+  filterInputFocus: React.PropTypes.func.isRequired
 };
 
 export default SidebarFilter;
