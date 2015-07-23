c6b9cf8d403924b9433f86df109119267cac5960
Ben Lodge <blodge@hubspot.com>
2015-07-23 11:25:23 -0400

    add zerclipboard so don't need to use cdnjs from react-zeroclipboard

--- BlazarUI/app/scripts/main.js
+++ BlazarUI/app/scripts/main.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
 import config from './config';
+import ZeroClipboard from 'ZeroClipboard';
 
 window.app = {
   config: config
 };
+
+// make ZeroClipboard global
+// for react-zeroclipboard
+window.ZeroClipboard = ZeroClipboard;
 
 Router.run(routes, Router.HistoryLocation, Handler => React.render(<Handler />, document.body));
