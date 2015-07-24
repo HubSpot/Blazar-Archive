133e4ed2ef116e0a0d1850cd018e1387b4e3b067
Ben Lodge <blodge@hubspot.com>
2015-07-24 07:57:41 -0400

    add loading props

--- BlazarUI/app/scripts/components/branch/Branch.jsx
+++ BlazarUI/app/scripts/components/branch/Branch.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import PageHeader from '../shared/PageHeader.jsx';
 import Breadcrumb from '../shared/Breadcrumb.jsx';
 import UIGrid from '../shared/grid/UIGrid.jsx';
@@ -6,9 +6,13 @@ import UIGridItem from '../shared/grid/UIGridItem.jsx';
 import PageHeadline from '../shared/PageHeadline.jsx';
 import ModulesTable from './ModulesTable.jsx';
 
-class Branch extends React.Component{
+class Branch extends Component{
 
   render() {
+    if (this.props.loading) {
+      return <div></div>;
+    }
+
     return (
       <div>
         <PageHeader>
@@ -28,11 +32,10 @@ class Branch extends React.Component{
 
 }
 
-Branch.defaultProps = { loading: true };
-
 Branch.propTypes = {
-  params: React.PropTypes.object.isRequired,
-  modules: React.PropTypes.array.isRequired
+  params: PropTypes.object.isRequired,
+  modules: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 export default Branch;
