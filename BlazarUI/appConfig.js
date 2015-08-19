7e4f4b03b670c0ee35f015c1e73959af20146576
Kevin Moses <kmoses@hubspot.com>
2015-08-19 10:42:00 -0400

    Set api url to /api only if we are proxying locally

--- BlazarUI/appConfig.js
+++ BlazarUI/appConfig.js
@@ -7,7 +7,7 @@ const appConfig = {
   appRoot: process.env.BLAZAR_APP_URI || '/blazar/ui',
   // If we have an env variable, gulp is proxying it through /api.
   // Otherwise we'll get it from localStorage later.
-  apiRoot: process.env.BLAZAR_API_URL ? '/api' : ''
+  apiRoot: process.env.BLAZAR_API_URL || ''
 }
 
 module.exports = appConfig;
