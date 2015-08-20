428b216a1de31bd22093cbbd752fa33639118c00
Kevin Moses <kmoses@hubspot.com>
2015-08-20 14:40:48 -0400

    Add pagination to builds table

--- BlazarUI/app/scripts/components/module/Module.jsx
+++ BlazarUI/app/scripts/components/module/Module.jsx
@@ -7,10 +7,27 @@ import BuildHistoryTable from './BuildHistoryTable.jsx';
 import PageHeadline from '../shared/PageHeadline.jsx';
 import SectionLoader from '../shared/SectionLoader.jsx';
 import Icon from '../shared/Icon.jsx';
+import Paginator from '../shared/Paginator.jsx';
 
 class Module extends Component {
 
+  constructor() {
+    this.state = {
+      pagination: {
+        perPage: 15,
+        page: 0
+      }
+    };
+  }
+
+  onPage(page) {
+    this.state.pagination.page = page;
+    this.forceUpdate();
+  }
+
   render() {
+    let pagination = this.state.pagination || {};
+    let paginated = Paginator.paginate(this.props.buildHistory, pagination);
 
     if (this.props.loading) {
       return (
@@ -33,17 +50,28 @@ class Module extends Component {
         <UIGrid>
           <UIGridItem size={12}>
             <BuildHistoryTable
-              buildHistory={this.props.buildHistory}
+              buildHistory={paginated.data}
               loading={this.props.loading}
             />
           </UIGridItem>
         </UIGrid>
+        <div className='pagination-outer'>
+            <Paginator
+              className='pagination'
+              ellipsesClassName='disabled'
+              selectedClassName='active'
+              page={paginated.page}
+              pages={paginated.amount}
+              beginPages={1}
+              endPages={1}
+              showPrevNext={true}
+              onSelect={this.onPage.bind(this)}>
+            </Paginator>
+        </div>
       </div>
     );
   }
-
 }
-
 
 Module.propTypes = {
   loading: PropTypes.bool.isRequired,
---
+++ BlazarUI/app/scripts/utils/range.js
@@ -0,0 +1,11 @@
+module.exports = function(a, b) {
+    var ret = [];
+    var i = b? a: 0;
+    var len = b? b: a;
+
+    for(; i < len; i++) {
+        ret.push(i);
+    }
+
+    return ret;
+};
