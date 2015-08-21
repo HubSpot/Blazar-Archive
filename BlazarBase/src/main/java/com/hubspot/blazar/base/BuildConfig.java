25c3b5ebce68bc376aa17eb7c5a56aa0c66346ec
Jonathan Goodwin <jgoodwin@hubspot.com>
2015-08-21 17:47:50 -0400

    add webhooks feild, dont use optionals as much

--- BlazarBase/src/main/java/com/hubspot/blazar/base/BuildConfig.java
+++ BlazarBase/src/main/java/com/hubspot/blazar/base/BuildConfig.java
@@ -1,57 +1,57 @@
 package com.hubspot.blazar.base;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.print.attribute.standard.MediaSize.Other;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class BuildConfig {
-  private List<String> cmds;
-  private final Optional<Map<String, String>> env;
-  private final Optional<List<String>> buildDependencies;
-  private final Optional<List<String>> buildDependents;
-  private final Optional<Boolean> autoDeployQa;
+
+  private final List<String> cmds;
+  private final Map<String, String> env;
+  private final List<String> buildDependencies;
+  private final List<String> buildDependents;
+  private final List<String> webHooks;
 
   @JsonCreator
-  public BuildConfig(@JsonProperty("cmds") Optional<List<String>> cmds,
-                     @JsonProperty("env") Optional<Map<String, String>> env,
-                     @JsonProperty("buildDependencies") Optional<List<String>> buildDependencies,
-                     @JsonProperty("buildDependents") Optional<List<String>> buildDependents,
-                     @JsonProperty("autoDeployQa") Optional<Boolean> autoDeployQa) {
-    this.env = env;
-    if (cmds.isPresent()) {
-      this.cmds = cmds.get();
-    } else{
-      this.cmds = ImmutableList.of("runbuildpack");
-    }
-    this.buildDependencies = buildDependencies;
-    this.buildDependents = buildDependents;
-    this.autoDeployQa = autoDeployQa;
+  public BuildConfig(@JsonProperty("cmds") List<String> cmds,
+                     @JsonProperty("env") Map<String, String> env,
+                     @JsonProperty("buildDependencies") List<String> buildDependencies,
+                     @JsonProperty("buildDependents") List<String> buildDependents,
+                     @JsonProperty("webHooks") List<String> webHooks) {
+
+    this.cmds = Objects.firstNonNull(cmds, ImmutableList.of("runbuildpack"));
+    this.env = Objects.firstNonNull(env, Collections.<String,String>emptyMap());
+    this.buildDependencies = Objects.firstNonNull(buildDependencies, Collections.<String>emptyList());
+    this.buildDependents = Objects.firstNonNull(buildDependents, Collections.<String>emptyList());
+    this.webHooks = Objects.firstNonNull(webHooks, Collections.<String>emptyList());
   }
 
   public List<String> getCmds() {
     return cmds;
   }
 
-  public Optional<Map<String, String>> getEnv() {
+  public Map<String, String> getEnv() {
     return env;
   }
 
-  public Optional<List<String>> getBuildDependencies() {
+  public List<String> getBuildDependencies() {
     return buildDependencies;
   }
 
-  public Optional<List<String>> getBuildDependents() {
+  public List<String> getBuildDependents() {
     return buildDependents;
   }
 
-  public Optional<Boolean> getAutoDeployQa() {
-    return autoDeployQa;
+  public List<String> getWebHooks() {
+    return webHooks;
   }
-
 }
