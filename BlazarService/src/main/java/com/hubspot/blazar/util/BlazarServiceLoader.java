e459cbf7ff3afa313f19d7a3facd362f08f5b160
Jonathan Haber <jhaber@hubspot.com>
2015-08-27 15:34:10 -0400

    Remove

--- BlazarService/src/main/java/com/hubspot/blazar/util/BlazarServiceLoader.java
+++ BlazarService/src/main/java/com/hubspot/blazar/util/BlazarServiceLoader.java
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 package com.hubspot.blazar.util;
 
 import java.io.BufferedReader;
