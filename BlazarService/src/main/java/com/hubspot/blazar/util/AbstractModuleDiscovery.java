840dfe801bbd9760779663bb69d0da828e5e5af1
Jonathan Haber <jhaber@hubspot.com>
2015-08-27 15:33:24 -0400

    Make module discovery extensible

---
+++ BlazarService/src/main/java/com/hubspot/blazar/util/AbstractModuleDiscovery.java
@@ -0,0 +1,52 @@
+package com.hubspot.blazar.util;
+
+import com.google.common.base.Preconditions;
+import com.hubspot.blazar.base.GitInfo;
+import com.hubspot.blazar.github.GitHubProtos.Commit;
+import com.hubspot.blazar.github.GitHubProtos.PushEvent;
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHTree;
+import org.kohsuke.github.GitHub;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractModuleDiscovery implements ModuleDiscovery {
+
+  @Inject
+  Map<String, GitHub> gitHubByHost;
+
+  protected String contentsFor(String file, GHRepository repository, GitInfo gitInfo) throws IOException {
+    GHContent content = repository.getFileContent(file, gitInfo.getBranch());
+    return content.getContent();
+  }
+
+  protected GHTree treeFor(GHRepository repository, GitInfo gitInfo) throws IOException {
+    return repository.getTreeRecursive(gitInfo.getBranch(), 1);
+  }
+
+  protected GHRepository repositoryFor(GitInfo gitInfo) throws IOException {
+    return gitHubFor(gitInfo).getRepository(gitInfo.getFullRepositoryName());
+  }
+
+  protected GitHub gitHubFor(GitInfo gitInfo) {
+    String host = gitInfo.getHost();
+
+    return Preconditions.checkNotNull(gitHubByHost.get(host), "No GitHub found for host " + host);
+  }
+
+  protected Set<String> affectedPaths(PushEvent pushEvent) {
+    Set<String> affectedPaths = new HashSet<>();
+    for (Commit commit : pushEvent.getCommitsList()) {
+      affectedPaths.addAll(commit.getAddedList());
+      affectedPaths.addAll(commit.getModifiedList());
+      affectedPaths.addAll(commit.getRemovedList());
+    }
+
+    return affectedPaths;
+  }
+}
---
+++ BlazarService/src/main/java/com/hubspot/blazar/util/MavenModuleDiscovery.java
@@ -0,0 +1,70 @@
+package com.hubspot.blazar.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.common.base.Optional;
+import com.hubspot.blazar.base.GitInfo;
+import com.hubspot.blazar.base.Module;
+import com.hubspot.blazar.github.GitHubProtos.PushEvent;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHTree;
+import org.kohsuke.github.GHTreeEntry;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Singleton
+public class MavenModuleDiscovery extends AbstractModuleDiscovery {
+  private final XmlMapper xmlMapper;
+
+  @Inject
+  public MavenModuleDiscovery(XmlMapper xmlMapper) {
+    this.xmlMapper = xmlMapper;
+  }
+
+  @Override
+  public boolean shouldRediscover(GitInfo gitInfo, PushEvent pushEvent) {
+    for (String path : affectedPaths(pushEvent)) {
+      if (isPom(path)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public Set<Module> discover(GitInfo gitInfo) throws IOException {
+    GHRepository repository = repositoryFor(gitInfo);
+    GHTree tree = treeFor(repository, gitInfo);
+
+    Set<String> poms = new HashSet<>();
+    for (GHTreeEntry entry : tree.getTree()) {
+      if (isPom(entry.getPath())) {
+        poms.add(entry.getPath());
+      }
+    }
+
+    Set<Module> modules = new HashSet<>();
+    for (String pom : poms) {
+      JsonNode node = xmlMapper.readTree(contentsFor(pom, repository, gitInfo));
+      String artifactId = node.get("artifactId").textValue();
+      final String glob;
+      if ("pom".equals(node.path("packaging").textValue())) {
+        glob = pom;
+      } else {
+        glob = (pom.contains("/") ? pom.substring(0, pom.lastIndexOf('/') + 1) : "") + "**";
+      }
+      modules.add(new Module(Optional.<Integer>absent(), artifactId, pom, glob, true));
+    }
+
+    return modules;
+  }
+
+  private static boolean isPom(String path) {
+    return "pom.xml".equals(path) || path.endsWith("/pom.xml");
+  }
+}
