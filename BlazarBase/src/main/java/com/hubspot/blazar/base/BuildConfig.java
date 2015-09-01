329d162e7fd6a175e3bde1a15aed4243c1e22803
Jonathan Haber <jhaber@hubspot.com>
2015-09-01 16:00:24 -0400

    Use buildDeps

--- BlazarBase/src/main/java/com/hubspot/blazar/base/BuildConfig.java
+++ BlazarBase/src/main/java/com/hubspot/blazar/base/BuildConfig.java
@@ -10,31 +10,26 @@ import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
 
 public class BuildConfig {
-
   private final List<String> cmds;
   private final Map<String, String> env;
-  private final List<String> buildDependencies;
-  private final List<String> buildDependents;
+  private final List<String> buildDeps;
   private final List<String> webhooks;
 
   @JsonCreator
   public BuildConfig(@JsonProperty("cmds") List<String> cmds,
                      @JsonProperty("env") Map<String, String> env,
-                     @JsonProperty("buildDependencies") List<String> buildDependencies,
-                     @JsonProperty("buildDependents") List<String> buildDependents,
+                     @JsonProperty("buildDeps") List<String> buildDeps,
                      @JsonProperty("webhooks") List<String> webhooks) {
 
     this.cmds = Objects.firstNonNull(cmds, ImmutableList.of("runbuildpack"));
     this.env = Objects.firstNonNull(env, Collections.<String,String>emptyMap());
-    this.buildDependencies = Objects.firstNonNull(buildDependencies, Collections.<String>emptyList());
-    this.buildDependents = Objects.firstNonNull(buildDependents, Collections.<String>emptyList());
+    this.buildDeps = Objects.firstNonNull(buildDeps, Collections.<String>emptyList());
     this.webhooks = Objects.firstNonNull(webhooks, Collections.<String>emptyList());
   }
 
   public static BuildConfig makeDefaultBuildConfig(){
-    return new BuildConfig(null, null, null, null, null);
+    return new BuildConfig(null, null, null, null);
   }
-
 
   public List<String> getCmds() {
     return cmds;
@@ -44,12 +39,8 @@ public class BuildConfig {
     return env;
   }
 
-  public List<String> getBuildDependencies() {
-    return buildDependencies;
-  }
-
-  public List<String> getBuildDependents() {
-    return buildDependents;
+  public List<String> getBuildDeps() {
+    return buildDeps;
   }
 
   public List<String> getWebhooks() {
