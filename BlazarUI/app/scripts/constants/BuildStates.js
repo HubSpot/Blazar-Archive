3059de170c7933abdcc4a4d3aa1f0c9e6c7bdd2a
Ben Lodge <blodge@hubspot.com>
2015-09-18 22:30:24 -0400

    add build state constants

---
+++ BlazarUI/app/scripts/constants/BuildStates.js
@@ -0,0 +1,10 @@
+import keyMirror from 'keymirror';
+
+export default keyMirror({
+  SUCCEEDED: null,
+  FAILED: null,
+  IN_PROGRESS: null,
+  CANCELLED: null,
+  LAUNCHING: null,
+  QUEUED: null
+});
