d07a647b1113396607143917d8b9e11acdce6c41
Jonathan Haber <HiJon89@Gmail.com>
2015-09-20 14:51:57 -0400

    Handle "changed" file status

--- BlazarService/src/main/java/com/hubspot/blazar/util/BuildLauncher.java
+++ BlazarService/src/main/java/com/hubspot/blazar/util/BuildLauncher.java
@@ -256,6 +256,7 @@ public class BuildLauncher {
           builder.addAdded(file.getFileName());
           break;
         case "modified":
+        case "changed":  
         case "renamed":
           builder.addModified(file.getFileName());
           break;
