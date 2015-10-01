f2ac0cbccdc482b33dd4cdb6297faf5754be54fe
Jonathan Haber <jhaber@hubspot.com>
2015-10-01 10:34:31 -0400

    Set the right log offsets

--- BlazarService/src/main/java/com/hubspot/blazar/resources/BuildResource.java
+++ BlazarService/src/main/java/com/hubspot/blazar/resources/BuildResource.java
@@ -32,6 +32,7 @@ import com.hubspot.singularity.client.SingularityClient;
 import com.sun.jersey.api.NotFoundException;
 
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Set;
 
@@ -98,7 +99,9 @@ public class BuildResource {
 
     Optional<MesosFileChunkObject> chunk = singularityClient.readSandBoxFile(taskId, path, grep, Optional.of(offset), Optional.of(length));
     if (chunk.isPresent()) {
-      return new LogChunk(chunk.get().getData(), chunk.get().getOffset(), chunk.get().getOffset() + length);
+      String data = chunk.get().getData();
+      offset = chunk.get().getOffset();
+      return new LogChunk(data, offset, offset + data.getBytes(StandardCharsets.UTF_8).length);
     } else {
       Collection<SingularityS3Log> s3Logs = singularityClient.getTaskLogs(taskId);
       if (s3Logs.isEmpty()) {
@@ -109,7 +112,7 @@ public class BuildResource {
 
       SingularityS3Log s3Log = s3Logs.iterator().next();
       if (offset >= s3Log.getSize()) {
-        return new LogChunk("", s3Log.getSize(), s3Log.getSize() + length);
+        return new LogChunk("", s3Log.getSize(), s3Log.getSize());
       }
 
       return fetchS3Log(s3Log.getGetUrl(), offset, length);
@@ -143,7 +146,8 @@ public class BuildResource {
 
     HttpResponse response = asyncHttpClient.execute(request).get();
     if (response.isSuccess()) {
-      return new LogChunk(response.getAsString(), offset, offset + length);
+      byte[] data = response.getAsBytes();
+      return new LogChunk(new String(data, StandardCharsets.UTF_8), offset, offset + data.length);
     } else {
       String message = String.format("Error reading S3 log, status code %d, response %s", response.getStatusCode(), response.getAsString());
       throw new WebApplicationException(Response.serverError().entity(message).type(MediaType.TEXT_PLAIN_TYPE).build());
