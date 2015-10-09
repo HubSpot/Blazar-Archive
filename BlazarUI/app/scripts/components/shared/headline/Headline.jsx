539ee7a4296697d9d57f0364c961498d306103d7
Ben Lodge <blodge@hubspot.com>
2015-10-09 09:50:40 -0400

    add optional border to headline

--- BlazarUI/app/scripts/components/shared/headline/Headline.jsx
+++ BlazarUI/app/scripts/components/shared/headline/Headline.jsx
@@ -1,19 +1,33 @@
 import React, {Component, PropTypes} from 'react';
+import ClassNames from 'classnames';
 
 class Headline extends Component{
 
+  getClassNames() {
+    return ClassNames([
+      'headline',
+      this.props.className,
+      {'border' : this.props.border}
+    ]);
+  }
+
   render() {
     return (
-      <h2 className={`headline ${this.props.className}`}>
+      <h2 className={this.getClassNames()}>
         {this.props.children}
       </h2>
     );
   }
 }
 
+Headline.defaultProps = {
+  border: false
+}
+
 Headline.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string
+  className: PropTypes.string,
+  border: PropTypes.bool
 };
 
 export default Headline;
