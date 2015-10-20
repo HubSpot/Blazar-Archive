06b06b536d0b4dd1c2830f98b2507c316c767e48
Jonathan Goodwin <jgoodwin@hubspot.com>
2015-10-20 17:27:59 -0400

    fix * import

--- BlazarService/src/main/java/com/hubspot/blazar/resources/PersonalizationResource.java
+++ BlazarService/src/main/java/com/hubspot/blazar/resources/PersonalizationResource.java
@@ -15,7 +15,12 @@ import org.kohsuke.github.GitHub;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.HashSet;
