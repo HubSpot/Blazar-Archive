85357f300ef0791e7b1d419066417b39c3cff7b9
Ben Lodge <blodge@hubspot.com>
2015-10-22 12:35:39 -0400

    create headline component so we can wait for star status to load

--- BlazarUI/app/scripts/components/module/ModuleContainer.jsx
+++ BlazarUI/app/scripts/components/module/ModuleContainer.jsx
@@ -1,16 +1,11 @@
 import React, {Component, PropTypes} from 'react';
 import {bindAll, clone, some} from 'underscore';
-import {getPathname} from '../Helpers';
+import ModuleHeadline from './ModuleHeadline.jsx';
 import PageContainer from '../shared/PageContainer.jsx';
-import {getIsStarredState} from '../Helpers.js';
 import UIGrid from '../shared/grid/UIGrid.jsx';
 import UIGridItem from '../shared/grid/UIGridItem.jsx';
 import BuildHistoryTable from './BuildHistoryTable.jsx';
-import Headline from '../shared/headline/Headline.jsx';
-import HeadlineDetail from '../shared/headline/HeadlineDetail.jsx';
-import Icon from '../shared/Icon.jsx';
 import BuildButton from './BuildButton.jsx';
-import Star from '../shared/Star.jsx';
 
 import BuildHistoryStore from '../../stores/buildHistoryStore';
 import BuildStore from '../../stores/buildStore';
@@ -23,7 +18,6 @@ import LocationStore from '../../stores/locationStore';
 let initialState = {
   buildHistory: [],
   stars: [],
-  buildTriggeringDone: true,
   loadingHistory: true,
   loadingStars: true,
   buildTriggeringError: ''
@@ -33,7 +27,7 @@ class ModuleContainer extends Component {
 
   constructor(props) {
     super(props);
-    bindAll(this, 'triggerBuild', 'onStatusChange', 'toggleStar');
+    bindAll(this, 'triggerBuild', 'onStatusChange');
     this.state = initialState;
   }
 
@@ -75,30 +69,18 @@ class ModuleContainer extends Component {
     BuildActions.triggerBuild(this.props.params.moduleId);
   }
 
-  toggleStar(isStarred, starInfo) {
-    StarActions.toggleStar(isStarred, starInfo);
-  }
+  
 
   render() {
     return (
       <PageContainer>
         <UIGrid>
           <UIGridItem size={10}>
-            <Headline>
-              <Star
-                className='icon-roomy'
-                isStarred={getIsStarredState(this.state.stars, this.props.params.moduleId)}
-                toggleStar={this.toggleStar}
-                modulePath={getPathname()}
-                moduleName={this.props.params.module}
-                moduleId={this.props.params.moduleId}
-                updateWithState={true}
-              />
-              {this.props.params.module} 
-              <HeadlineDetail>
-                Build History
-              </HeadlineDetail>
-            </Headline>
+            <ModuleHeadline
+              params={this.props.params}
+              stars={this.state.stars}
+              loading={this.state.loadingStars || this.state.loadingHistory}
+            />
           </UIGridItem>           
           <UIGridItem size={2} align='RIGHT'>
             <BuildButton 
