fc7fe08ed0870b8ee20c33363bd91ae2a166fc1b
Ben Lodge <blodge@hubspot.com>
2015-11-18 10:30:27 -0500

    move xhr to actions and create store

---
+++ BlazarUI/app/scripts/actions/feedbackActions.js
@@ -0,0 +1,18 @@
+import Reflux from 'reflux';
+import Feedback from '../models/Feedback';
+
+const FeedbackActions = Reflux.createActions([
+  { 'sendFeedback': {children: ['completed','failed']} }
+]);
+
+FeedbackActions.sendFeedback.listen( function(payload) {
+  const feedback = new Feedback(payload);
+  // to do: implement reflux catch method, waiting on an issue response.
+  feedback.submit()
+    .done(() => { this.completed();})
+    .fail((jqXHR, textStatus, errorThrown) => {
+      this.failed(`Status ${jqXHR.status}: ${errorThrown}`);
+    });
+});
+
+export default FeedbackActions;
