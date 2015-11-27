5af6da71cc16fb876048829c3f732808212f1289
Ben Lodge <blodge@hubspot.com>
2015-11-27 14:09:48 -0500

    add optional emphasis to log message

--- BlazarUI/app/scripts/components/build/BuildLogLine.jsx
+++ BlazarUI/app/scripts/components/build/BuildLogLine.jsx
@@ -1,18 +1,27 @@
 import React, {Component, PropTypes} from 'react';
+import ClassNames from 'classnames';
 
 class BuildLogLine extends Component {
+
+  getClassNames() {
+    return ClassNames([
+      'log-line',
+      {'log-line-emphasis' : this.props.emphasis}
+    ])
+  }
 
   render() {
     const offset = `offset-${this.props.offset}`;
     return (
-      <p id={offset} className='log-line'>{this.props.text}</p>
+      <p id={offset} className={this.getClassNames()}>{this.props.text}</p>
     );
   }
 }
 
 BuildLogLine.propTypes = {
   text: PropTypes.string,
-  offset: PropTypes.number
+  offset: PropTypes.number,
+  emphasis: PropTypes.bool
 };
 
 export default BuildLogLine;
