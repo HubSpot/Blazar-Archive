da2f410bbfd6d6fe4eb9836f8e4e12724cbe9994
Jonathan Haber <jhaber@hubspot.com>
2015-12-11 15:35:21 -0500

    Implement log endpoints

--- BlazarService/src/main/java/com/hubspot/blazar/resources/ModuleBuildResource.java
+++ BlazarService/src/main/java/com/hubspot/blazar/resources/ModuleBuildResource.java
@@ -5,27 +5,52 @@ import com.hubspot.blazar.base.LogChunk;
 import com.hubspot.blazar.base.ModuleBuild;
 import com.hubspot.blazar.base.ModuleBuild.State;
 import com.hubspot.blazar.data.service.ModuleBuildService;
+import com.hubspot.horizon.AsyncHttpClient;
+import com.hubspot.horizon.HttpRequest;
+import com.hubspot.horizon.HttpResponse;
+import com.hubspot.mesos.json.MesosFileChunkObject;
+import com.hubspot.singularity.SingularityS3Log;
+import com.hubspot.singularity.SingularityTaskHistory;
+import com.hubspot.singularity.SingularityTaskHistoryUpdate;
+import com.hubspot.singularity.SingularityTaskHistoryUpdate.SimplifiedTaskState;
+import com.hubspot.singularity.client.SingularityClient;
 import com.sun.jersey.api.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Path("/modules/builds")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ModuleBuildResource {
+  private static final Logger LOG = LoggerFactory.getLogger(ModuleBuildResource.class);
+
   private final ModuleBuildService moduleBuildService;
+  private final SingularityClient singularityClient;
+  private final AsyncHttpClient asyncHttpClient;
 
   @Inject
-  public ModuleBuildResource(ModuleBuildService moduleBuildService) {
+  public ModuleBuildResource(ModuleBuildService moduleBuildService,
+                             SingularityClient singularityClient,
+                             AsyncHttpClient asyncHttpClient) {
     this.moduleBuildService = moduleBuildService;
+    this.singularityClient = singularityClient;
+    this.asyncHttpClient = asyncHttpClient;
   }
 
   @GET
@@ -70,25 +95,123 @@ public class ModuleBuildResource {
 
   @GET
   @Path("/{id}/log")
-  public LogChunk getLog(@PathParam("id") long moduleBuildId) {
-    // TODO
-    return null;
+  public LogChunk getLog(@PathParam("id") long moduleBuildId,
+                         @QueryParam("offset") @DefaultValue("0") long offset,
+                         @QueryParam("length") @DefaultValue("65536") long length) throws Exception {
+    ModuleBuild build = getBuildWithError(moduleBuildId);
+
+    Optional<String> taskId = build.getTaskId();
+    if (!taskId.isPresent()) {
+      throw new NotFoundException("No taskId found for build: " + moduleBuildId);
+    }
+
+    String path = taskId.get() + "/service.log";
+    Optional<String> grep = Optional.absent();
+
+    Optional<MesosFileChunkObject> chunk = singularityClient.readSandBoxFile(taskId.get(), path, grep, Optional.of(offset), Optional.of(length));
+    if (chunk.isPresent()) {
+      if (chunk.get().getData().isEmpty() && logCompleted(build)) {
+        return new LogChunk(chunk.get().getData(), chunk.get().getOffset(), -1);
+      } else {
+        return new LogChunk(chunk.get().getData(), chunk.get().getOffset());
+      }
+    } else {
+      SingularityS3Log s3Log = findS3ServiceLog(taskId.get());
+      if (offset >= s3Log.getSize()) {
+        return new LogChunk("", s3Log.getSize(), -1);
+      }
+
+      return readS3LogChunk(s3Log.getGetUrl(), offset, length);
+    }
   }
 
   @GET
   @Path("/{id}/log/size")
   public Object getLogSize(@PathParam("id") long moduleBuildId) {
-    // TODO
-    return null;
+    ModuleBuild build = getBuildWithError(moduleBuildId);
+
+    Optional<String> taskId = build.getTaskId();
+    if (!taskId.isPresent()) {
+      throw new NotFoundException("No taskId found for build: " + moduleBuildId);
+    }
+
+    String path = taskId.get() + "/service.log";
+    Optional<Long> absent = Optional.absent();
+    Optional<String> grep = Optional.absent();
+
+    Optional<MesosFileChunkObject> chunk = singularityClient.readSandBoxFile(taskId.get(), path, grep, absent, absent);
+    if (chunk.isPresent()) {
+      return chunk.get().getOffset();
+    } else {
+      return findS3ServiceLog(taskId.get()).getSize();
+    }
+  }
+
+  private SingularityS3Log findS3ServiceLog(String taskId) {
+    Collection<SingularityS3Log> s3Logs = singularityClient.getTaskLogs(taskId);
+    List<SingularityS3Log> serviceLogs = new ArrayList<>();
+    for (SingularityS3Log s3Log : s3Logs) {
+      if (s3Log.getGetUrl().contains("service.log")) {
+        serviceLogs.add(s3Log);
+      }
+    }
+
+    if (serviceLogs.isEmpty()) {
+      throw new NotFoundException("No S3 log found for task " + taskId);
+    } else if (serviceLogs.size() > 1) {
+      throw new NotFoundException("Multiple S3 logs found for task " + taskId);
+    } else {
+      return serviceLogs.get(0);
+    }
+  }
+
+  private LogChunk readS3LogChunk(String url, long offset, long length) throws Exception {
+    HttpRequest request = HttpRequest.newBuilder()
+        .setUrl(url)
+        .addHeader("Range", String.format("bytes=%d-%d", offset, offset + length - 1))
+        .build();
+
+    HttpResponse response = asyncHttpClient.execute(request).get();
+    if (response.isSuccess()) {
+      return new LogChunk(response.getAsBytes(), offset);
+    } else {
+      String message = String.format("Error reading S3 log, status code %d, response %s", response.getStatusCode(), response.getAsString());
+      LOG.warn(message);
+      throw new WebApplicationException(Response.serverError().entity(message).type(MediaType.TEXT_PLAIN_TYPE).build());
+    }
+  }
+
+  private boolean logCompleted(ModuleBuild build) {
+    if (!build.getState().isComplete()) {
+      return false;
+    } else if (build.getState() != State.CANCELLED) {
+      return true;
+    } else {
+      return taskComplete(build.getTaskId().get());
+    }
+  }
+
+  private boolean taskComplete(String taskId) {
+    Optional<SingularityTaskHistory> taskHistory = singularityClient.getHistoryForTask(taskId);
+    if (!taskHistory.isPresent()) {
+      return true;
+    }
+
+    SimplifiedTaskState taskState = SingularityTaskHistoryUpdate.getCurrentState(taskHistory.get().getTaskUpdates());
+    return taskState == SimplifiedTaskState.DONE;
+  }
+
+  private ModuleBuild getBuildWithError(long moduleBuildId) {
+    Optional<ModuleBuild> maybeBuild = get(moduleBuildId);
+    if (maybeBuild.isPresent()) {
+      return maybeBuild.get();
+    } else {
+      throw new NotFoundException("No build found with id: " + moduleBuildId);
+    }
   }
 
   private ModuleBuild getBuildWithExpectedState(long moduleBuildId, State expected) {
-    Optional<ModuleBuild> maybeBuild = get(moduleBuildId);
-    if (!maybeBuild.isPresent()) {
-      throw new NotFoundException("No build found with id: " + moduleBuildId);
-    }
-
-    ModuleBuild build = maybeBuild.get();
+    ModuleBuild build = getBuildWithError(moduleBuildId);
     if (build.getState() == expected) {
       return build;
     } else {
