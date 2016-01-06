1a2f23f7c59c08da72a3e94c4a92981443b04c06
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-01-06 17:57:59 -0500

    some tweaks per comments

--- BlazarBase/src/main/java/com/hubspot/blazar/base/BuildCommand.java
+++ BlazarBase/src/main/java/com/hubspot/blazar/base/BuildCommand.java
@@ -1,42 +1,41 @@
 package com.hubspot.blazar.base;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 
 public class BuildCommand {
-  private static String DEFAULT_CMD = "/bin/bash";
-  private static String DEFAULT_ARG = "-c";
+  private static final String DEFAULT_CMD = "/bin/bash";
+  private static final String DEFAULT_ARG = "-c";
+  public static final Set<Integer> DEFAULT_RETURN_CODES = ImmutableSet.of(0);
 
   private final String executable;
   private final List<String> args;
-  private final List<Integer> returnCodes;
+  private final Set<Integer> returnCodes;
   private final Map<String, String> commandSpecificEnvironment;
 
   @JsonCreator
   public BuildCommand(@JsonProperty("executable") String executable,
                       @JsonProperty("args") List<String> args,
-                      @JsonProperty("returnCodes") List<Integer> returnCodes,
+                      @JsonProperty("returnCodes") Set<Integer> returnCodes,
                       @JsonProperty("env") Map<String, String> commandSpecificEnvironment) {
     this.executable = Objects.firstNonNull(executable, DEFAULT_CMD);
     this.args = Objects.firstNonNull(args, Lists.newArrayList(DEFAULT_ARG));
-    this.returnCodes = Objects.firstNonNull(returnCodes, Lists.newArrayList(0));
+    this.returnCodes = Objects.firstNonNull(returnCodes, DEFAULT_RETURN_CODES);
     this.commandSpecificEnvironment = Objects.firstNonNull(commandSpecificEnvironment, new HashMap<String, String>());
   }
 
   @JsonCreator
   public static BuildCommand buildCommandFromString(String cmd) {
-    String executable = DEFAULT_CMD;
-    List<String> args = Lists.newArrayList(DEFAULT_ARG, cmd);
-    List<Integer> returnCodes = Lists.newArrayList(0);
-    Map<String, String> env = new HashMap<String, String>();
-
-    return new BuildCommand(executable, args, returnCodes, env);
+    return new BuildCommand(DEFAULT_CMD, Lists.newArrayList(DEFAULT_ARG, cmd), DEFAULT_RETURN_CODES, Collections.EMPTY_MAP);
   }
 
   public String getExecutable() {
@@ -47,10 +46,11 @@ public class BuildCommand {
     return args;
   }
 
-  public List<Integer> getReturnCodes() {
+  public Set<Integer> getReturnCodes() {
     return returnCodes;
   }
 
+  @JsonProperty("env")
   public Map<String, String> getCommandSpecificEnvironment() {
     return commandSpecificEnvironment;
   }
