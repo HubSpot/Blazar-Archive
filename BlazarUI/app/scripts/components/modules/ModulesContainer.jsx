4692fd1a8b817dccffe64cc75c121d2ff3adb61b
Ben Lodge <blodge@hubspot.com>
2016-01-06 16:06:01 -0500

    add new modules page to display module builds by repo build

--- BlazarUI/app/scripts/components/modules/ModulesContainer.jsx
+++ BlazarUI/app/scripts/components/modules/ModulesContainer.jsx
@@ -5,9 +5,17 @@ import UIGrid from '../shared/grid/UIGrid.jsx';
 import UIGridItem from '../shared/grid/UIGridItem.jsx';
 import GenericErrorMessage from '../shared/GenericErrorMessage.jsx';
 
+import StarStore from '../../stores/starStore';
+import StarActions from '../../actions/starActions';
+import ModulesHeadline from './ModulesHeadline.jsx';
+
 
 let initialState = {
-
+  modules: [],
+  stars: [],
+  loadingModules: true,
+  loadingStars: true,
+  buildTriggeringError: ''
 };
 
 class ModulesContainer extends Component {
@@ -15,6 +23,35 @@ class ModulesContainer extends Component {
   constructor(props) {
     super(props);
     this.state = initialState;
+    
+    bindAll(this, 'onStatusChange')
+  }
+  
+  componentDidMount() {
+    this.setup(this.props.params);
+  }
+
+  componentWillReceiveProps(nextprops) {
+    this.tearDown()
+    this.setup(nextprops.params);
+    this.setState(initialState);
+  }
+
+  componentWillUnmount() {
+    this.tearDown()
+  }
+  
+  setup(params) { 
+    this.unsubscribeFromStars = StarStore.listen(this.onStatusChange);
+    StarActions.loadStars('moduleContainer');
+  }
+  
+  tearDown() {
+    this.unsubscribeFromStars();
+  }
+  
+  onStatusChange(state) {
+    this.setState(state);
   }
 
   renderSectionContent() {
@@ -42,8 +79,12 @@ class ModulesContainer extends Component {
   renderPage() {
     return (
       <UIGrid>
-        <UIGridItem size={12}>
-          Modules here...
+        <UIGridItem size={10}>
+          <ModulesHeadline
+            params={this.props.params}
+            stars={this.state.stars}
+            loading={false}
+          />
         </UIGridItem>
       </UIGrid>
     );
