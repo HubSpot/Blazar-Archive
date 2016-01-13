1fdcb8e3d0cc10ccf11d48a0c6a4d42cd465b656
Jonathan Haber <jhaber@hubspot.com>
2016-01-13 10:32:08 -1000

    Add remaining bindings

--- BlazarData/src/test/java/com/hubspot/blazar/data/BlazarDataTestModule.java
+++ BlazarData/src/test/java/com/hubspot/blazar/data/BlazarDataTestModule.java
@@ -1,9 +1,15 @@
 package com.hubspot.blazar.data;
 
 import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.eventbus.EventBus;
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.hubspot.jackson.datatype.protobuf.ProtobufModule;
 import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.jackson.Jackson;
 
 public class BlazarDataTestModule implements Module {
 
@@ -11,8 +17,8 @@ public class BlazarDataTestModule implements Module {
   public void configure(Binder binder) {
     binder.bind(DataSourceFactory.class).toInstance(buildDataSourceFactory());
     binder.bind(MetricRegistry.class).toInstance(new MetricRegistry());
-
-    // TODO: EventBus, ObjectMapper
+    binder.bind(ObjectMapper.class).toInstance(buildObjectMapper());
+    binder.bind(EventBus.class).toInstance(buildEventBus());
   }
 
   private DataSourceFactory buildDataSourceFactory() {
@@ -23,5 +29,22 @@ public class BlazarDataTestModule implements Module {
     dataSourceFactory.setPassword("password");
 
     return dataSourceFactory;
+  }
+
+  private ObjectMapper buildObjectMapper() {
+    return Jackson.newObjectMapper()
+        .registerModule(new ProtobufModule())
+        .setSerializationInclusion(Include.NON_NULL)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  }
+
+  private EventBus buildEventBus() {
+    return new EventBus() {
+
+      @Override
+      public void post(Object event) {
+        // TODO
+      }
+    };
   }
 }
