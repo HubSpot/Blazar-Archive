3449fd466f13ca36a6500876dd8b69ca1e5e7a6e
Ben Lodge <blodge@hubspot.com>
2016-01-19 16:17:16 -0500

    filter repos by org

--- BlazarUI/app/scripts/data/OrgApi.js
+++ BlazarUI/app/scripts/data/OrgApi.js
@@ -1,4 +1,4 @@
-import { findWhere, uniq, sortBy } from 'underscore';
+import { findWhere, uniq, sortBy, filter } from 'underscore';
 import { fromJS } from 'immutable';
 import StoredBuilds from './StoredBuilds';
 
@@ -7,20 +7,27 @@ class OrgApi extends StoredBuilds {
   _parse() {
     const {params} = this.options;
     
-    let repos = uniq(this.builds.toJS().map((build) => {
+    // get unique repos 
+    const repos = uniq(this.builds.toJS().map((build) => {
       return {
+        organization: build.gitInfo.organization,
         repository: build.gitInfo.repository,
         blazarRepositoryPath: build.gitInfo.blazarRepositoryPath
       };
     }), (b) => {
       return b.repository;
     });
+
+    // filter by org
+    const reposFiltered = repos.filter((repo) => {
+      return repo.organization === params.org;
+    });
     
-    repos = sortBy(repos, (r) => {
+    const sortedRepos = sortBy(reposFiltered, (r) => {
       return r.repository.toLowerCase();
     });
 
-    this.cb(fromJS(repos));
+    this.cb(fromJS(sortedRepos));
   }
 
 }
--- BlazarUI/app/scripts/stores/orgStore.js
+++ BlazarUI/app/scripts/stores/orgStore.js
@@ -7,9 +7,7 @@ const RepoStore = Reflux.createStore({
   listenables: OrgActions,
 
   onLoadRepos(params) {
-    this.orgApi = new OrgApi({params});
-
-    this.orgApi.fetchBuilds((repos) => {
+    this.orgApi = new OrgApi({params}).fetchBuilds((repos) => {
       this.trigger({
         repos: repos,
         loading: false
