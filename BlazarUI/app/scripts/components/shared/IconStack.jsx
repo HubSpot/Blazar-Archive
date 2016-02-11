58f5e29ee032acbe13860e1b185dfbfe7df7901e
Zachary Hickman <zhickman@hubspot.com>
2016-02-11 11:40:21 -0500

    actually fix indentation in file

--- BlazarUI/app/scripts/components/shared/IconStack.jsx
+++ BlazarUI/app/scripts/components/shared/IconStack.jsx
@@ -11,38 +11,38 @@ class IconStack extends Component {
       this.props.classNames
     ]);
   }
-	
-	renderIconStackBase() {
-		return (
-			<Icon type="fa" classNames={this.getBaseClassNames()} name={this.props.iconStackBase} />
-		);
-	}
+  
+  renderIconStackBase() {
+    return (
+      <Icon type="fa" classNames={this.getBaseClassNames()} name={this.props.iconStackBase} />
+    );
+  }
 
-	renderIcon(iconName) {
-		return (
-			<Icon type="fa" classNames="fa-stack-1x" name={iconName} />
-		);
-	}
+  renderIcon(iconName) {
+    return (
+      <Icon type="fa" classNames="fa-stack-1x" name={iconName} />
+    );
+  }
 
-	render() {
-		return (
-			<span className="fa-icon-stack">
-				{this.renderIconStackBase()}
-				{this.props.iconNames.map(iconName => this.renderIcon(iconName))}
-			</span>
-		);
-	}
+  render() {
+    return (
+      <span className="fa-icon-stack">
+        {this.renderIconStackBase()}
+        {this.props.iconNames.map(iconName => this.renderIcon(iconName))}
+      </span>
+    );
+  }
 }
 
 IconStack.defaultProps = {
-	iconStackBase: '',
-	iconNames: Immutable.List.of(),
+  iconStackBase: '',
+  iconNames: Immutable.List.of(),
   classNames: Immutable.List.of()
 }
 
 IconStack.propTypes = {
-	iconStackBase: PropTypes.string,
-	iconNames: PropTypes.instanceOf(Immutable.List),
+  iconStackBase: PropTypes.string,
+  iconNames: PropTypes.instanceOf(Immutable.List),
   classNames: PropTypes.instanceOf(Immutable.List)
 }
 
