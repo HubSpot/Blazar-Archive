bfd62f5451ada595f75a43cc9b841b90a54b9d99
Zachary Hickman <zhickman@hubspot.com>
2016-02-22 16:17:20 -0500

    running timer for in progress builds

--- BlazarUI/app/scripts/components/Helpers.js
+++ BlazarUI/app/scripts/components/Helpers.js
@@ -247,7 +247,7 @@ export const filterInactiveBuilds = function(builds) {
 };
 
 export const getTableDurationText = function(state, duration) {
-  if (contains(ACTIVE_BUILD_STATES, state) || state === BuildStates.SKIPPED) {
+  if ((state !== BuildStates.IN_PROGRESS && contains(ACTIVE_BUILD_STATES, state)) || state === BuildStates.SKIPPED) {
     return humanizeText(state);
   }
 
