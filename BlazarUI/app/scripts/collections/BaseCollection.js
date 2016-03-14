f3e8fbb10172d2285d182843d89c2000a3f5fa72
Zachary Hickman <zhickman@hubspot.com>
2016-03-14 18:45:05 -0400

    encode the branch

--- BlazarUI/app/scripts/collections/BaseCollection.js
+++ BlazarUI/app/scripts/collections/BaseCollection.js
@@ -21,25 +21,25 @@ class BaseCollection extends Collection {
 
       if (has(item, 'build')) {
         item.build.duration = humanizeDuration(item.build.endTimestamp - item.build.startTimestamp, {round: true});
-        item.build.blazarPath = `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${gitInfo.branch}/${module.name}/${item.build.buildNumber}`.replace('#', '%23');
+        item.build.blazarPath = `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${encodeURIComponentCo(gitInfo.branch)}/${module.name}/${item.build.buildNumber}`.replace('#', '%23');
       }
 
       if (has(item, 'module')) {
         item.module.blazarPath = {
-          module: `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${gitInfo.branch}/${module.name}`.replace('#', '%23'),
-          branch: `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${gitInfo.branch}`.replace('#', '%23'),
+          module: `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${encodeURIComponent(gitInfo.branch)}/${module.name}`.replace('#', '%23'),
+          branch: `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${encodeURIComponent(gitInfo.branch)}`.replace('#', '%23'),
           repo: `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}`
         };
       }
 
       if (has(item, 'inProgressBuild')) {
-        item.inProgressBuild.blazarPath = `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${gitInfo.branch}/${module.name}/${inProgressBuild.buildNumber}`.replace('#', '%23');
+        item.inProgressBuild.blazarPath = `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${encodeURIComponent(gitInfo.branch)}/${module.name}/${inProgressBuild.buildNumber}`.replace('#', '%23');
         item.inProgressBuild.duration = humanizeDuration(Date.now() - item.inProgressBuild.startTimestamp, {round: true});
       }
 
       if (has(item, 'lastBuild')) {
         item.lastBuild.duration = humanizeDuration(item.lastBuild.endTimestamp - item.lastBuild.startTimestamp, {round: true});
-        item.lastBuild.blazarPath = `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${gitInfo.branch}/${module.name}/${lastBuild.buildNumber}`.replace('#', '%23');
+        item.lastBuild.blazarPath = `/builds/${gitInfo.host}/${gitInfo.organization}/${gitInfo.repository}/${encodeURIComponent(gitInfo.branch)}/${module.name}/${lastBuild.buildNumber}`.replace('#', '%23');
       }
 
       return item;
