9662135a9783710758e3b36c32afb28307c6ed41
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-03-14 12:40:29 -0400

    Static deps & provides

--- BlazarService/src/main/java/com/hubspot/blazar/discovery/hsstatic/HubSpotStaticModuleDiscovery.java
+++ BlazarService/src/main/java/com/hubspot/blazar/discovery/hsstatic/HubSpotStaticModuleDiscovery.java
@@ -3,6 +3,7 @@ package com.hubspot.blazar.discovery.hsstatic;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -12,6 +13,7 @@ import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHTree;
 import org.kohsuke.github.GHTreeEntry;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Optional;
 import com.hubspot.blazar.base.CommitInfo;
 import com.hubspot.blazar.base.DependencyInfo;
@@ -28,10 +30,12 @@ public class HubSpotStaticModuleDiscovery implements ModuleDiscovery {
   private static final String STATIC_CONF = "static/static_conf.json";
 
   private final GitHubHelper gitHubHelper;
+  private final ObjectMapper objectMapper;
 
   @Inject
-  public HubSpotStaticModuleDiscovery(GitHubHelper gitHubHelper) {
+  public HubSpotStaticModuleDiscovery(GitHubHelper gitHubHelper, ObjectMapper objectMapper) {
     this.gitHubHelper = gitHubHelper;
+    this.objectMapper = objectMapper;
   }
 
   @Override
@@ -49,7 +53,6 @@ public class HubSpotStaticModuleDiscovery implements ModuleDiscovery {
   public DiscoveryResult discover(GitInfo gitInfo) throws IOException {
     GHRepository repository = gitHubHelper.repositoryFor(gitInfo);
     GHTree tree = gitHubHelper.treeFor(repository, gitInfo);
-
     Set<String> staticConfigs = new HashSet<>();
     for (GHTreeEntry entry : tree.getTree()) {
       if (isStaticConfig(entry.getPath())) {
@@ -60,21 +63,23 @@ public class HubSpotStaticModuleDiscovery implements ModuleDiscovery {
     Set<DiscoveredModule> modules = new HashSet<>();
     Set<MalformedFile> malformedFiles = new HashSet<>();
     for (String config : staticConfigs) {
-      String moduleDirectory = String.valueOf(Paths.get(config).getParent());
-      String moduleName = moduleName(gitInfo, moduleDirectory);
+      String contents = gitHubHelper.contentsFor(config, repository, gitInfo);
+      StaticConfig configObject = objectMapper.readValue(contents, StaticConfig.class);
+      String moduleDirectory = Paths.get(config).getParent().toString();
       String glob = (moduleDirectory.contains("/") ? moduleDirectory.substring(0, moduleDirectory.lastIndexOf('/') + 1) : "") + "**";
-      modules.add(new DiscoveredModule(moduleName, "hs-static", moduleDirectory, glob, BUILD_CONFIGURATION, DependencyInfo.unknown()));
+      modules.add(new DiscoveredModule(configObject.getName(), "hs-static", moduleDirectory, glob, BUILD_CONFIGURATION, makeDeps(configObject)));
     }
     return new DiscoveryResult(modules, malformedFiles);
   }
 
-  private static String moduleName(GitInfo gitInfo, String path) {
-    return path.contains("/") ? folderName(path) : gitInfo.getRepository();
-  }
-
-  private static String folderName(String path) {
-    String folderPath = path.substring(0, path.lastIndexOf('/'));
-    return folderPath.contains("/") ? folderPath.substring(folderPath.lastIndexOf('/') + 1) : folderPath;
+  private static DependencyInfo makeDeps(StaticConfig config) {
+    Set<String> depends = new HashSet<>();
+    for (Map.Entry<String, Integer> entry: config.getDeps().entrySet()) {
+      depends.add(String.format("%s-%d", entry.getKey(), entry.getValue()));
+    }
+    Set<String> provides = new HashSet<>();
+    provides.add(String.format("%s-%d", config.getName(), config.getMajorVersion()));
+    return new DependencyInfo(depends, provides);
   }
 
   private static boolean isStaticConfig(String path) {
