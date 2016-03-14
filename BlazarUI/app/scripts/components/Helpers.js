e6c7fb4a245305a3ea93ce8782ed8e218bb73ed9
Zachary Hickman <zhickman@hubspot.com>
2016-03-14 12:45:46 -0400

    one more location

--- BlazarUI/app/scripts/components/Helpers.js
+++ BlazarUI/app/scripts/components/Helpers.js
@@ -23,7 +23,7 @@ export const timestampFormatted = function(timestamp, format='lll') {
 };
 
 export const timestampDuration = function(startTimestamp, endTimestamp, round='true') {
-  return humanizeDuration(endTimestamp - startTimestamp, {round: round, units: ['h', 'm', 's']});
+  return humanizeDuration(endTimestamp - startTimestamp, {round: round});
 };
 
 // 'BUILD_SUCCEEEDED' => 'Build Succeeded'
