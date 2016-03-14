23db5795e6f9eb50220871a3044775a96b29178e
Zachary Hickman <zhickman@hubspot.com>
2016-03-14 09:17:45 -0400

    indexlink

--- BlazarUI/app/scripts/pages/notFound.jsx
+++ BlazarUI/app/scripts/pages/notFound.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PageContainer from '../components/shared/PageContainer.jsx';
-import { Link } from 'react-router';
+import { IndexLink } from 'react-router';
 
 class NotFound extends Component {
 
@@ -9,7 +9,7 @@ class NotFound extends Component {
       <PageContainer classNames='not-found-container'>
         <h1 className="notfound__heading">404. Page Not Found. <br/> Sorry about that.</h1>
         <p>
-          <Link to='dashboard'>Head back to the dashboard</Link>
+          <IndexLink to='dashboard'>Head back to the dashboard</IndexLink>
         </p>
       </PageContainer>
     );
