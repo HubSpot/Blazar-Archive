ae0bedbe30a520b69186762a2a49c0be0739720c
Zachary Hickman <zhickman@hubspot.com>
2016-03-14 09:33:50 -0400

    try out this fancy new thing

--- BlazarUI/app/scripts/main.js
+++ BlazarUI/app/scripts/main.js
@@ -1,13 +1,18 @@
 /*global config*/
 import React from 'react';
 import { render } from 'react-dom';
-import {Router, browserHistory} from 'react-router';
+import { createHistory } from 'history';
+import { Router, useRouterHistory } from 'react-router';
 import routes from './routes';
 
 if (!config.apiRoot) {
   console.warn('You need to set your apiRoot via localStorage');
   console.warn('e.g. localStorage["apiRootOverride"] = "https://path.to-api.com/v1/api"');
 }
+
+const browserHistory = useRouterHistory(createHistory) ({
+  basename: config.appRoot
+});
 
 render(
 	<Router history={browserHistory}>
--- BlazarUI/app/scripts/routes.js
+++ BlazarUI/app/scripts/routes.js
@@ -12,21 +12,15 @@ import Build from './pages/build.jsx';
 import RepoBuild from './pages/repoBuild.jsx';
 import NotFound from './pages/notFound.jsx';
 
-function pagePath(path) {
-  return config.appRoot + path;
-}
-
 const routes = (
   <Route name='app' path='/' component={App}>
     <IndexRoute name='dashboard' component={ Dashboard } />
-    <Route path={config.appRoot}>
-      <Route name='org' path='/builds/:host/:org' component={Org} />
-      <Route name='repo' path='/builds/:host/:org/:repo' component={Repo} />
-      <Route name='branch' path='/builds/:host/:org/:repo/:branch' component={Branch} />
-      <Route name='repoBuild' path='/builds/:host/:org/:repo/:branch/:buildNumber' component={RepoBuild} />
-      <Route name='build' path='/builds/:host/:org/:repo/:branch/:buildNumber/:moduleName' component={Build} />
-      <Route name='notFound' path="*" component={ NotFound } />
-    </Route>
+    <Route name='org' path='/builds/:host/:org' component={Org} />
+    <Route name='repo' path='/builds/:host/:org/:repo' component={Repo} />
+    <Route name='branch' path='/builds/:host/:org/:repo/:branch' component={Branch} />
+    <Route name='repoBuild' path='/builds/:host/:org/:repo/:branch/:buildNumber' component={RepoBuild} />
+    <Route name='build' path='/builds/:host/:org/:repo/:branch/:buildNumber/:moduleName' component={Build} />
+    <Route name='notFound' path="*" component={ NotFound } />
   </Route>
 );
 
