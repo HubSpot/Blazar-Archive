e2a219904e5ab9ccb06bb9a006a52dbf405afc7c
Zachary Hickman <zhickman@hubspot.com>
2016-04-04 10:10:46 -0400

    more progress yay

--- BlazarUI/app/scripts/data/NewRepoBuildApi.js
+++ BlazarUI/app/scripts/data/NewRepoBuildApi.js
@@ -2,6 +2,15 @@
 import Resource from '../services/ResourceProvider';
 import { findWhere } from 'underscore';
 
+function _fetchModuleNames(params) {
+  const moduleNamesPromise = new Resource({
+    url: `${config.apiRoot}/branches/${params.branchId}/modules`,
+    type: 'GET'
+  }).send();
+
+  return moduleNamesPromise;
+}
+
 function _fetchBranchBuildHistory(params) {
   const branchBuildHistoryPromise = new Resource({
     url: `${config.apiRoot}/builds/history/branch/${params.branchId}`,
@@ -35,6 +44,24 @@ function fetchModuleBuilds(params, cb) {
 
     return moduleBuildsPromise.then((resp) => {
       console.log(resp);
+      return cb(resp);
+    });
+  })
+}
+
+function fetchModuleBuilds(params, cb) {
+  return _fetchBranchBuildHistory(params).then((resp) => {
+    const repoBuild = findWhere(resp, {buildNumber: parseInt(params.buildNumber, 10)});
+    const moduleBuildsPromise = new Resource({
+      url: `${config.apiRoot}/branches/builds/${repoBuild.id}/modules`,
+      type: 'GET'
+    }).send();
+
+    _fetchModuleNames(params).then((resp) => {
+      console.log("modules: ", resp);
+    });
+
+    return moduleBuildsPromise.then((resp) => {
       return cb(resp);
     });
   })
