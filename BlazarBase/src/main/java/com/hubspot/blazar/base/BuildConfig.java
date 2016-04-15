12ed9f7d11d1e88901be71d6e7f9e26ee24c9650
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-04-15 18:07:34 -0400

    allow you configure depends / provides in your .blazar.yaml

--- BlazarBase/src/main/java/com/hubspot/blazar/base/BuildConfig.java
+++ BlazarBase/src/main/java/com/hubspot/blazar/base/BuildConfig.java
@@ -3,6 +3,7 @@ package com.hubspot.blazar.base;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -19,6 +20,8 @@ public class BuildConfig {
   private final Optional<GitInfo> buildpack;
   private final Optional<String> user;
   private final Map<String, StepActivationCriteria> stepActivation;
+  private final Set<String> depends;
+  private final Set<String> provides;
 
   @JsonCreator
   public BuildConfig(@JsonProperty("steps") List<BuildStep> steps,
@@ -29,7 +32,9 @@ public class BuildConfig {
                      @JsonProperty("cache") List<String> cache,
                      @JsonProperty("buildpack") Optional<GitInfo> buildpack,
                      @JsonProperty("user") Optional<String> user,
-                     @JsonProperty("stepActivation") Map<String, StepActivationCriteria> stepActivation) {
+                     @JsonProperty("stepActivation") Map<String, StepActivationCriteria> stepActivation,
+                     @JsonProperty("depends") Set<String> depends,
+                     @JsonProperty("provides") Set<String> provides) {
     this.steps = Objects.firstNonNull(steps, Collections.<BuildStep>emptyList());
     this.before = Objects.firstNonNull(before, Collections.<BuildStep>emptyList());
     this.env = Objects.firstNonNull(env, Collections.<String,String>emptyMap());
@@ -39,10 +44,12 @@ public class BuildConfig {
     this.cache = Objects.firstNonNull(cache, Collections.<String>emptyList());
     this.user = Objects.firstNonNull(user, Optional.<String>absent());
     this.stepActivation = Objects.firstNonNull(stepActivation, Collections.<String, StepActivationCriteria>emptyMap());
+    this.depends = Objects.firstNonNull(depends, Collections.<String>emptySet());
+    this.provides = Objects.firstNonNull(provides, Collections.<String>emptySet());
   }
 
   public static BuildConfig makeDefaultBuildConfig(){
-    return new BuildConfig(null, null, null, null, null, null, null, null, null);
+    return new BuildConfig(null, null, null, null, null, null, null, null, null, null, null);
   }
 
   public List<BuildStep> getSteps() {
@@ -81,8 +88,16 @@ public class BuildConfig {
     return stepActivation;
   }
 
+  public Set<String> getProvides() {
+    return provides;
+  }
+
+  public Set<String> getDepends() {
+    return depends;
+  }
+
   public BuildConfig withUser(String user) {
-    return new BuildConfig(steps, before, env, buildDeps, webhooks, cache, buildpack, Optional.of(user), stepActivation);
+    return new BuildConfig(steps, before, env, buildDeps, webhooks, cache, buildpack, Optional.of(user), stepActivation, depends, provides);
   }
 
 }
