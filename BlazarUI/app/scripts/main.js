acc602891f9224fe7d973c1c49b3361bbff84b8f
Zachary Hickman <zhickman@hubspot.com>
2016-04-19 10:24:31 -0400

    check cookie not config key

--- BlazarUI/app/scripts/main.js
+++ BlazarUI/app/scripts/main.js
@@ -4,14 +4,15 @@ import { render } from 'react-dom';
 import { createHistory } from 'history';
 import { Router, useRouterHistory } from 'react-router';
 import routes from './routes';
+import Cookies from 'js-cookie';
 
 if (!config.apiRoot) {
   console.warn('You need to set your apiRoot via localStorage');
   console.warn('e.g. localStorage["apiRootOverride"] = "https://path.to-api.com/v1/api"');
 }
 
-if (config.fullstoryToken && config.usernameCookie) {
-  FS.identify(config.usernameCookie, {
+if (config.fullstoryToken && Cookies.get(config.usernameCookie)) {
+  FS.identify(Cookies.get(config.usernameCookie), {
     app: 'blazar'
   });
 }
