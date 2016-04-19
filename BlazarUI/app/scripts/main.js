b00953da85543ed44543539b5b12f34a846fb2b9
Zachary Hickman <zhickman@hubspot.com>
2016-04-19 10:34:32 -0400

    temporary testing

--- BlazarUI/app/scripts/main.js
+++ BlazarUI/app/scripts/main.js
@@ -12,10 +12,17 @@ if (!config.apiRoot) {
 }
 
 if (config.fullstoryToken && Cookies.get(config.usernameCookie)) {
+  console.log("Got in the if");
   FS.identify(Cookies.get(config.usernameCookie), {
     app: 'blazar'
   });
+} else {
+  console.log("Did not get in the if");
 }
+
+console.log("Fullstory token: ", config.fullstoryToken);
+console.log("Cookie key: ", config.usernameCookie);
+console.log("Cookie val: ", Cookies.get(config.usernameCookie));
 
 const browserHistory = useRouterHistory(createHistory) ({
   basename: config.appRoot
