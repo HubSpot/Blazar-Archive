cf2368347815bf75107f5455075276617e8357e2
Grigorios Chomatas <gchomatas@hubspot.com>
2016-04-28 13:22:20 -0400

    Fixed test subclass binding

--- BlazarService/src/test/java/com/hubspot/blazar/BlazarServiceTestModule.java
+++ BlazarService/src/test/java/com/hubspot/blazar/BlazarServiceTestModule.java
@@ -1,14 +1,13 @@
 package com.hubspot.blazar;
 
+import static org.mockito.Mockito.*;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.kohsuke.github.BlazarGHRepository;
 import org.kohsuke.github.BlazarGitHub;
-
-import com.hubspot.blazar.listener.TestBuildLauncher;
-import com.hubspot.blazar.test.base.service.BlazarGitTestConfiguration;
 import org.kohsuke.github.GitHub;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +16,8 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.google.common.eventbus.EventBus;
 import com.google.common.io.Resources;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.google.inject.multibindings.MapBinder;
 import com.google.inject.multibindings.Multibinder;
 import com.hubspot.blazar.base.visitor.ModuleBuildVisitor;
@@ -26,13 +27,13 @@ import com.hubspot.blazar.data.BlazarDataModule;
 import com.hubspot.blazar.discovery.DiscoveryModule;
 import com.hubspot.blazar.integration.slack.SlackClient;
 import com.hubspot.blazar.listener.BuildVisitorModule;
+import com.hubspot.blazar.listener.TestBuildLauncher;
+import com.hubspot.blazar.test.base.service.BlazarGitTestConfiguration;
 import com.hubspot.blazar.test.base.service.BlazarTestModule;
 import com.hubspot.blazar.util.SingularityBuildLauncher;
 import com.hubspot.blazar.util.TestSingularityBuildLauncher;
 import com.hubspot.horizon.AsyncHttpClient;
 import com.hubspot.singularity.client.SingularityClient;
-
-import static org.mockito.Mockito.mock;
 
 public class BlazarServiceTestModule extends AbstractModule {
   private static final Logger LOG = LoggerFactory.getLogger(BlazarServiceTestModule.class);
@@ -47,12 +48,17 @@ public class BlazarServiceTestModule extends AbstractModule {
     bind(EventBus.class).toInstance(eventBus);
     bind(BlazarConfiguration.class).toInstance(buildBlazarConfiguration());
     bind(SingularityClient.class).toInstance(mock(SingularityClient.class));
-    bind(SingularityBuildLauncher.class).to(TestSingularityBuildLauncher.class);
     bind(SlackClient.class).toInstance(mock(SlackClient.class));
     bind(AsyncHttpClient.class).toInstance(mock(AsyncHttpClient.class));
     Multibinder<ModuleBuildVisitor> moduleBuildVisitors = Multibinder.newSetBinder(binder(), ModuleBuildVisitor.class);
     moduleBuildVisitors.addBinding().to(TestBuildLauncher.class);
     bindGitHubMap(); // does its own binding
+  }
+
+  @Singleton
+  @Provides
+  public SingularityBuildLauncher providesSingularityBuildLauncher(TestSingularityBuildLauncher testSingularityBuildLauncher) {
+    return testSingularityBuildLauncher;
   }
 
   private BlazarConfiguration buildBlazarConfiguration() {
@@ -70,7 +76,7 @@ public class BlazarServiceTestModule extends AbstractModule {
     MapBinder mapBinder = MapBinder.newMapBinder(binder(), String.class, GitHub.class);
     final BlazarGitTestConfiguration blazarGitTestConfiguration;
     try {
-       blazarGitTestConfiguration = new YAMLMapper().readValue(Resources.getResource("GitHubData.yaml").openStream(), BlazarGitTestConfiguration.class);
+      blazarGitTestConfiguration = new YAMLMapper().readValue(Resources.getResource("GitHubData.yaml").openStream(), BlazarGitTestConfiguration.class);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
