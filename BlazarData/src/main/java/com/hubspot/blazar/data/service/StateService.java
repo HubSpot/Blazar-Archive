843ba64f22b3274d2c3682569802c982c7a64a43
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-07-22 17:40:10 -0400

    branch state by module no gitInfo included

--- BlazarData/src/main/java/com/hubspot/blazar/data/service/StateService.java
+++ BlazarData/src/main/java/com/hubspot/blazar/data/service/StateService.java
@@ -1,21 +1,26 @@
 package com.hubspot.blazar.data.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import com.google.common.base.Optional;
+import com.hubspot.blazar.base.Module;
 import com.hubspot.blazar.base.ModuleState;
 import com.hubspot.blazar.base.RepositoryState;
 import com.hubspot.blazar.data.dao.StateDao;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import java.util.Set;
-
 @Singleton
 public class StateService {
   private final StateDao stateDao;
+  private ModuleService moduleService;
 
   @Inject
-  public StateService(StateDao stateDao) {
+  public StateService(StateDao stateDao, ModuleService moduleService) {
     this.stateDao = stateDao;
+    this.moduleService = moduleService;
   }
 
   public Set<RepositoryState> getAllRepositoryStates() {
@@ -31,6 +36,13 @@ public class StateService {
   }
 
   public Set<ModuleState> getModuleStatesByBranch(int branchId) {
-    return stateDao.getModuleStatesByBranch(branchId);
+    Set<ModuleState> states = new HashSet<>();
+    for (Module m : moduleService.getByBranch(branchId)) {
+      Optional<ModuleState> state = stateDao.getModuleStateById(m);
+      if (state.isPresent()) {
+        states.add(state.get());
+      }
+    }
+    return states;
   }
 }
