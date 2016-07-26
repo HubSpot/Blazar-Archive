ba2469f1d9bc10305e609879f6923ee960cfc99d
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-07-26 13:39:35 -0400

    use threadpool with pool of 2

--- BlazarService/src/main/java/com/hubspot/blazar/guice/VersionBackFillCommand.java
+++ BlazarService/src/main/java/com/hubspot/blazar/guice/VersionBackFillCommand.java
@@ -1,8 +1,14 @@
 package com.hubspot.blazar.guice;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import net.sourceforge.argparse4j.inf.Namespace;
 
@@ -27,10 +33,12 @@ public class VersionBackFillCommand extends ConfiguredCommand<BlazarConfiguratio
   private static String COMMAND_NAME = "version_backfill";
   private static String COMMAND_DESC = "Finds projects with no version data for their dependencies, and updates their versions";
   private static final Logger LOG = LoggerFactory.getLogger(VersionBackFillCommand.class);
+  private final ExecutorService executorService;
 
   @Inject
   public VersionBackFillCommand() {
     super(COMMAND_NAME, COMMAND_DESC);
+    this.executorService = Executors.newFixedThreadPool(2);
   }
 
   @Override
@@ -39,42 +47,68 @@ public class VersionBackFillCommand extends ConfiguredCommand<BlazarConfiguratio
                      BlazarConfiguration configuration) throws Exception {
 
     Injector i = Guice.createInjector(new VersionBackFillCommandModule(bootstrap, configuration));
-
     CompositeModuleDiscovery compositeModuleDiscovery = i.getInstance(CompositeModuleDiscovery.class);
     DependenciesService dependenciesService = i.getInstance(DependenciesService.class);
     ModuleDiscoveryService moduleDiscoveryService = i.getInstance(ModuleDiscoveryService.class);
 
     Set<GitInfo> toBeReDiscovered = dependenciesService.getBranchesWithNonVersionedDependencies();
     Set<GitInfo> failed = new HashSet<>();
+    Map<GitInfo, Future<Boolean>> futureMap = new HashMap<>();
     for (GitInfo branch : toBeReDiscovered) {
+      Task task = new Task(branch, compositeModuleDiscovery, moduleDiscoveryService);
+      Future<Boolean> future = executorService.submit(task);
+      futureMap.put(branch, future);
+    }
+
+    for (Map.Entry<GitInfo, Future<Boolean>> entry : futureMap.entrySet()) {
+      handleResult(failed, entry.getKey(), entry.getValue());
+    }
+  }
+
+  private class Task implements Callable<Boolean> {
+
+    private final GitInfo branch;
+    private final CompositeModuleDiscovery compositeModuleDiscovery;
+    private final ModuleDiscoveryService moduleDiscoveryService;
+
+    Task(GitInfo branch, CompositeModuleDiscovery compositeModuleDiscovery, ModuleDiscoveryService moduleDiscoveryService) {
+      this.branch = branch;
+      this.compositeModuleDiscovery = compositeModuleDiscovery;
+      this.moduleDiscoveryService = moduleDiscoveryService;
+    }
+
+    @Override
+    public Boolean call() throws Exception {
       LOG.info("Starting to process gitInfo {}", branch.toString());
-      boolean success;
       try {
-        success = discover(branch, compositeModuleDiscovery, moduleDiscoveryService);
+        DiscoveryResult result = compositeModuleDiscovery.discover(branch);
+        LOG.info("Got Discovery result: {}", result);
+        Set<Module> modules = moduleDiscoveryService.handleDiscoveryResult(branch, result);
+        LOG.info("Discovered modules {}", modules);
+        return true;
+      } catch (IOException e) {
+        LOG.info("Failed to discover branch {}", branch.toString(), e);
+        return false;
+      }
+    }
+  }
+
+  private void handleResult(Set<GitInfo> failed, GitInfo branch, Future<Boolean> future) {
+      boolean success;
+      LOG.info("Collecting future for branch {}", branch.toString());
+      try {
+        success = future.get();
       } catch (Exception e) {
         success = false;
         LOG.error("Exception while processing branch {}", branch, e);
       }
+
       if (success) {
         LOG.info("Completed re-discovery of {}", branch.toString());
       } else {
         LOG.info("Failed to process branch {}", branch.toString());
         failed.add(branch);
       }
-    }
     LOG.info("Failed to process the following branches {}", failed);
-  }
-
-  private boolean discover(GitInfo gitinfo, CompositeModuleDiscovery compositeModuleDiscovery, ModuleDiscoveryService moduleDiscoveryService) {
-    try {
-      DiscoveryResult result = compositeModuleDiscovery.discover(gitinfo);
-      LOG.info("Got Discovery result: {}", result);
-      Set<Module> modules = moduleDiscoveryService.handleDiscoveryResult(gitinfo, result);
-      LOG.info("Discovered modules {}", modules);
-      return true;
-    } catch (IOException e) {
-      LOG.info("Failed to discover branch {}", gitinfo.toString(), e);
-      return false;
-    }
   }
 }
