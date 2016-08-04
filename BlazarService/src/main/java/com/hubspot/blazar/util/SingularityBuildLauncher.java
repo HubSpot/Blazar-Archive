bec462da7dd9622d933f8c2386804fdcfc4558cb
Jonathan W Goodwin <jonathanwgoodwin@gmail.com>
2016-08-04 18:36:08 -0400

    Revert "Add support for bumping up the resources on your build"

--- BlazarService/src/main/java/com/hubspot/blazar/util/SingularityBuildLauncher.java
+++ BlazarService/src/main/java/com/hubspot/blazar/util/SingularityBuildLauncher.java
@@ -1,17 +1,15 @@
 package com.hubspot.blazar.util;
 
-import java.util.Arrays;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
 import com.google.common.base.Optional;
 import com.hubspot.blazar.base.ModuleBuild;
-import com.hubspot.blazar.base.externalservice.mesos.Resources;
 import com.hubspot.blazar.config.BlazarConfiguration;
 import com.hubspot.blazar.config.SingularityConfiguration;
 import com.hubspot.singularity.api.SingularityRunNowRequest;
 import com.hubspot.singularity.client.SingularityClient;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Arrays;
 
 @Singleton
 public class SingularityBuildLauncher {
@@ -31,18 +29,11 @@ public class SingularityBuildLauncher {
   private SingularityRunNowRequest buildRequest(ModuleBuild build) {
     String buildId = Long.toString(build.getId().get());
 
-    Optional<com.hubspot.mesos.Resources> mesosResources = Optional.absent();
-    Optional<Resources> blazarResources = build.getBuildConfig().get().getBuildResources();
-    if (blazarResources.isPresent()) {
-      mesosResources = Optional.of(Resources.toMesosResources(blazarResources.get()));
-    }
-
     return new SingularityRunNowRequest(
         Optional.of("Running Blazar module build " + buildId),
         Optional.of(false),
         Optional.of(buildId),
-        Optional.of(Arrays.asList("--buildId", buildId)),
-        mesosResources
+        Optional.of(Arrays.asList("--buildId", buildId))
     );
   }
 }
