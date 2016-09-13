c97c367577546b9894846ff145c5c8afbcece006
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-09-13 17:23:52 -0400

    hikari v1

--- BlazarData/src/main/resources/schema.sql
+++ BlazarData/src/main/resources/schema.sql
@@ -1,197 +1,198 @@
 --liquibase formatted sql
 
 --changeset tpetr:1
-CREATE TABLE "branches" (
-  "id" int(11) unsigned NOT NULL AUTO_INCREMENT,
-  "host" varchar(250) NOT NULL,
-  "organization" varchar(250) NOT NULL,
-  "repository" varchar(250) NOT NULL,
-  "repositoryId" int(11) unsigned NOT NULL,
-  "branch" varchar(250) NOT NULL,
-  "active" tinyint(1) NOT NULL DEFAULT 1,
-  "pendingBuildId" bigint(20) unsigned,
-  "inProgressBuildId" bigint(20) unsigned,
-  "lastBuildId" bigint(20) unsigned,
-  "createdTimestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updatedTimestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX ("repositoryId", "branch"),
-  INDEX ("updatedTimestamp")
+
+CREATE TABLE `branches` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `host` varchar(250) NOT NULL,
+  `organization` varchar(250) NOT NULL,
+  `repository` varchar(250) NOT NULL,
+  `repositoryId` int(11) unsigned NOT NULL,
+  `branch` varchar(250) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `pendingBuildId` bigint(20) unsigned,
+  `inProgressBuildId` bigint(20) unsigned,
+  `lastBuildId` bigint(20) unsigned,
+  `createdTimestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedTimestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX (`repositoryId`, `branch`),
+  INDEX (`updatedTimestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE "modules" (
-  "id" int(11) unsigned NOT NULL AUTO_INCREMENT,
-  "branchId" int(11) unsigned NOT NULL,
-  "name" varchar(250) NOT NULL,
-  "type" varchar(250) NOT NULL,
-  "path" varchar(250) NOT NULL,
-  "glob" varchar(250) NOT NULL,
-  "active" tinyint(1) NOT NULL DEFAULT 1,
-  "pendingBuildId" bigint(20) unsigned,
-  "inProgressBuildId" bigint(20) unsigned,
-  "lastBuildId" bigint(20) unsigned,
-  "createdTimestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updatedTimestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "buildpack" mediumtext,
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX ("branchId", "name", "type"),
-  INDEX ("updatedTimestamp")
+CREATE TABLE `modules` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `branchId` int(11) unsigned NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `type` varchar(250) NOT NULL,
+  `path` varchar(250) NOT NULL,
+  `glob` varchar(250) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `pendingBuildId` bigint(20) unsigned,
+  `inProgressBuildId` bigint(20) unsigned,
+  `lastBuildId` bigint(20) unsigned,
+  `createdTimestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedTimestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `buildpack` mediumtext,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX (`branchId`, `name`, `type`),
+  INDEX (`updatedTimestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE "module_provides" (
-  "moduleId" int(11) unsigned NOT NULL,
-  "name" varchar(250) NOT NULL,
-  PRIMARY KEY ("moduleId", "name"),
-  INDEX ("name")
+CREATE TABLE `module_provides` (
+  `moduleId` int(11) unsigned NOT NULL,
+  `name` varchar(250) NOT NULL,
+  PRIMARY KEY (`moduleId`, `name`),
+  INDEX (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE "module_depends" (
-  "moduleId" int(11) unsigned NOT NULL,
-  "name" varchar(250) NOT NULL,
-  PRIMARY KEY ("moduleId", "name"),
-  INDEX ("name")
+CREATE TABLE `module_depends` (
+  `moduleId` int(11) unsigned NOT NULL,
+  `name` varchar(250) NOT NULL,
+  PRIMARY KEY (`moduleId`, `name`),
+  INDEX (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE "repo_builds" (
-  "id" bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  "branchId" int(11) unsigned NOT NULL,
-  "buildNumber" int(11) unsigned NOT NULL,
-  "state" varchar(40) NOT NULL,
-  "buildTrigger" mediumtext NOT NULL,
-  "buildOptions" mediumtext,
-  "startTimestamp" bigint(20) unsigned,
-  "endTimestamp" bigint(20) unsigned,
-  "commitInfo" mediumtext,
-  "dependencyGraph" mediumtext,
-  "sha" char(40),
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX ("branchId", "buildNumber")
+CREATE TABLE `repo_builds` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `branchId` int(11) unsigned NOT NULL,
+  `buildNumber` int(11) unsigned NOT NULL,
+  `state` varchar(40) NOT NULL,
+  `buildTrigger` mediumtext NOT NULL,
+  `buildOptions` mediumtext,
+  `startTimestamp` bigint(20) unsigned,
+  `endTimestamp` bigint(20) unsigned,
+  `commitInfo` mediumtext,
+  `dependencyGraph` mediumtext,
+  `sha` char(40),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX (`branchId`, `buildNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE "module_builds" (
-  "id" bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  "moduleId" int(11) unsigned NOT NULL,
-  "repoBuildId" bigint(20) unsigned NOT NULL,
-  "buildNumber" int(11) unsigned NOT NULL,
-  "state" varchar(40) NOT NULL,
-  "startTimestamp" bigint(20) unsigned,
-  "endTimestamp" bigint(20) unsigned,
-  "buildConfig" mediumtext,
-  "resolvedConfig" mediumtext,
-  "taskId" varchar(500),
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX ("moduleId", "buildNumber"),
-  INDEX ("repoBuildId"),
-  INDEX ("state")
+CREATE TABLE `module_builds` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `moduleId` int(11) unsigned NOT NULL,
+  `repoBuildId` bigint(20) unsigned NOT NULL,
+  `buildNumber` int(11) unsigned NOT NULL,
+  `state` varchar(40) NOT NULL,
+  `startTimestamp` bigint(20) unsigned,
+  `endTimestamp` bigint(20) unsigned,
+  `buildConfig` mediumtext,
+  `resolvedConfig` mediumtext,
+  `taskId` varchar(500),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX (`moduleId`, `buildNumber`),
+  INDEX (`repoBuildId`),
+  INDEX (`state`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE "malformed_files" (
-  "branchId" int(11) unsigned NOT NULL,
-  "type" varchar(250) NOT NULL,
-  "path" varchar(250) NOT NULL,
-  "details" mediumtext,
-  PRIMARY KEY ("branchId", "type", "path")
+CREATE TABLE `malformed_files` (
+  `branchId` int(11) unsigned NOT NULL,
+  `type` varchar(250) NOT NULL,
+  `path` varchar(250) NOT NULL,
+  `details` mediumtext,
+  PRIMARY KEY (`branchId`, `type`, `path`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --changeset jhaber:2 dbms:mysql
-ALTER TABLE "branches" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `branches` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
-ALTER TABLE "branches" MODIFY "branch" varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `branches` MODIFY `branch` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE "branches" MODIFY "updatedTimestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+ALTER TABLE `branches` MODIFY `updatedTimestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
-ALTER TABLE "modules" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `modules` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
-ALTER TABLE "modules" MODIFY "updatedTimestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+ALTER TABLE `modules` MODIFY `updatedTimestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
-ALTER TABLE "module_provides" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `module_provides` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
-ALTER TABLE "module_depends" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `module_depends` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
-ALTER TABLE "repo_builds" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `repo_builds` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
-ALTER TABLE "repo_builds" MODIFY "commitInfo" mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `repo_builds` MODIFY `commitInfo` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE "repo_builds" MODIFY "buildOptions" mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `repo_builds` MODIFY `buildOptions` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE "module_builds" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `module_builds` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
-ALTER TABLE "malformed_files" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `malformed_files` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
 --changeset jhaber:3 dbms:h2
-CREATE TRIGGER "upd_branch_timestamp" AFTER UPDATE ON "branches" FOR EACH ROW CALL "com.hubspot.blazar.test.base.data.UpdateTimestampTrigger";
+CREATE TRIGGER `upd_branch_timestamp` AFTER UPDATE ON `branches` FOR EACH ROW CALL `com.hubspot.blazar.test.base.data.UpdateTimestampTrigger`;
 
-CREATE TRIGGER "upd_module_timestamp" AFTER UPDATE ON "modules" FOR EACH ROW CALL "com.hubspot.blazar.test.base.data.UpdateTimestampTrigger";
+CREATE TRIGGER `upd_module_timestamp` AFTER UPDATE ON `modules` FOR EACH ROW CALL `com.hubspot.blazar.test.base.data.UpdateTimestampTrigger`;
 
 --changeset jhaber:4 dbms:h2 runAlways:true
-TRUNCATE TABLE "branches";
+TRUNCATE TABLE `branches`;
 
-TRUNCATE TABLE "modules";
+TRUNCATE TABLE `modules`;
 
-TRUNCATE TABLE "module_provides";
+TRUNCATE TABLE `module_provides`;
 
-TRUNCATE TABLE "module_depends";
+TRUNCATE TABLE `module_depends`;
 
-TRUNCATE TABLE "repo_builds";
+TRUNCATE TABLE `repo_builds`;
 
-TRUNCATE TABLE "module_builds";
+TRUNCATE TABLE `module_builds`;
 
-TRUNCATE TABLE "malformed_files";
+TRUNCATE TABLE `malformed_files`;
 
 --changeset jgoodwin:5 runAlways:false
-CREATE TABLE "instant_message_configs" (
-  "id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  "branchId" BIGINT(20) UNSIGNED NOT NULL,
-  "moduleId" BIGINT(20) UNSIGNED,
-  "channelName" varchar(21),
-  "onFinish" TINYINT(1),
-  "onFail" TINYINT(1),
-  "onChange" TINYINT(1),
-  "onRecover" TINYINT(1),
-  "active" TINYINT(1),
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX ("channelName", "branchId", "moduleId"),
-  INDEX ("channelName")
+CREATE TABLE `instant_message_configs` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `branchId` BIGINT(20) UNSIGNED NOT NULL,
+  `moduleId` BIGINT(20) UNSIGNED,
+  `channelName` varchar(21),
+  `onFinish` TINYINT(1),
+  `onFail` TINYINT(1),
+  `onChange` TINYINT(1),
+  `onRecover` TINYINT(1),
+  `active` TINYINT(1),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX (`channelName`, `branchId`, `moduleId`),
+  INDEX (`channelName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --changeset jgoodwin:6 runAlways:false
 CREATE TABLE inter_project_builds (
-  "id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  "state" VARCHAR(40) NOT NULL,
-  "moduleIds" MEDIUMTEXT NOT NULL,
-  "buildTrigger" MEDIUMTEXT NOT NULL,
-  "startTimestamp" BIGINT(20) UNSIGNED,
-  "endTimestamp" BIGINT(20) UNSIGNED,
-  "dependencyGraph" MEDIUMTEXT,
-  PRIMARY KEY ("id"),
-  INDEX("state")
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `state` VARCHAR(40) NOT NULL,
+  `moduleIds` MEDIUMTEXT NOT NULL,
+  `buildTrigger` MEDIUMTEXT NOT NULL,
+  `startTimestamp` BIGINT(20) UNSIGNED,
+  `endTimestamp` BIGINT(20) UNSIGNED,
+  `dependencyGraph` MEDIUMTEXT,
+  PRIMARY KEY (`id`),
+  INDEX(`state`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE inter_project_build_mappings (
-  "id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  "interProjectBuildId" BIGINT(20),
-  "branchId" INT(11),
-  "repoBuildId" BIGINT(20),
-  "moduleId" INT(11),
-  "moduleBuildId" BIGINT(20),
-  "state" varchar(40) NOT NULL,
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX ("moduleBuildId"),
-  INDEX ("interProjectBuildId"),
-  INDEX ("repoBuildId")
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `interProjectBuildId` BIGINT(20),
+  `branchId` INT(11),
+  `repoBuildId` BIGINT(20),
+  `moduleId` INT(11),
+  `moduleBuildId` BIGINT(20),
+  `state` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX (`moduleBuildId`),
+  INDEX (`interProjectBuildId`),
+  INDEX (`repoBuildId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --changeset jgoodwin:7 runAlwasy:false
 CREATE TABLE branch_settings (
-  "branchId" INT(11),
-  "triggerInterProjectBuilds" TINYINT(1),
-  "interProjectBuildOptIn" TINYINT(1),
-  PRIMARY KEY ("branchId")
+  `branchId` INT(11),
+  `triggerInterProjectBuilds` TINYINT(1),
+  `interProjectBuildOptIn` TINYINT(1),
+  PRIMARY KEY (`branchId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --changeset jgoodwin:8
-ALTER TABLE "module_provides" ADD COLUMN "version" VARCHAR(190);
-ALTER TABLE "module_depends" ADD COLUMN "version" VARCHAR(190);
+ALTER TABLE `module_provides` ADD COLUMN `version` VARCHAR(190);
+ALTER TABLE `module_depends` ADD COLUMN `version` VARCHAR(190);
 
 --changeset jgoodwin:9 dbms:mysql
 ALTER TABLE `module_provides` MODIFY  `version` VARCHAR(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -200,19 +201,19 @@ ALTER TABLE `module_provides` MODIFY `name` VARCHAR(190) CHARACTER SET utf8mb4 C
 ALTER TABLE `module_depends` MODIFY `version` VARCHAR(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `module_depends` MODIFY `name` VARCHAR(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE "branch_settings" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
-ALTER TABLE "instant_message_configs" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
-ALTER TABLE "inter_project_build_mappings" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
-ALTER TABLE "inter_project_builds" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
-ALTER TABLE "malformed_files" ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `branch_settings` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `instant_message_configs` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `inter_project_build_mappings` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `inter_project_builds` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
+ALTER TABLE `malformed_files` ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;
 
 --changeset jgoodwin:10 dbms:h2
-ALTER TABLE "module_provides" MODIFY "version" VARCHAR(190) NOT NULL;
-ALTER TABLE "module_depends" MODIFY "version" VARCHAR(190) NOT NULL;
-ALTER TABLE "module_provides" DROP PRIMARY KEY;
-ALTER TABLE "module_depends" DROP PRIMARY KEY;
-ALTER TABLE "module_provides" ADD PRIMARY KEY("moduleId", "name", "version");
-ALTER TABLE "module_depends" ADD PRIMARY KEY("moduleId", "name", "version");
+ALTER TABLE `module_provides` MODIFY `version` VARCHAR(190) NOT NULL;
+ALTER TABLE `module_depends` MODIFY `version` VARCHAR(190) NOT NULL;
+ALTER TABLE `module_provides` DROP PRIMARY KEY;
+ALTER TABLE `module_depends` DROP PRIMARY KEY;
+ALTER TABLE `module_provides` ADD PRIMARY KEY(`moduleId`, `name`, `version`);
+ALTER TABLE `module_depends` ADD PRIMARY KEY(`moduleId`, `name`, `version`);
 --changeset jgoodwin:10 dbms:mysql
 ALTER TABLE `module_provides` DROP PRIMARY KEY, ADD PRIMARY KEY(`moduleId`, `name`, `version`);
 ALTER TABLE `module_depends` DROP PRIMARY KEY, ADD PRIMARY KEY(`moduleId`, `name`, `version`);
---
+++ BlazarTestBase/src/main/java/com/hubspot/blazar/test/base/data/HikariDataSourceFactory.java
@@ -0,0 +1,87 @@
+package com.hubspot.blazar.test.base.data;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.MetricRegistry;
+import com.mysql.management.MysqldResource;
+import com.mysql.management.MysqldResourceI;
+import com.zaxxer.hikari.HikariConfig;
+
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.db.ManagedDataSource;
+
+public class HikariDataSourceFactory extends DataSourceFactory {
+  private static final Logger LOG = LoggerFactory.getLogger(HikariDataSourceFactory.class);
+
+  @Override
+  public ManagedDataSource build(MetricRegistry metricRegistry, String name) {
+
+    final Path baseDir = Paths.get(System.getProperty("java.io.tmpdir"), "mysqld_" + UUID.randomUUID().toString());
+    int port = new Random().nextInt(20000) + 3207;
+
+    final MysqldResource mysqldResource = new MysqldResource(baseDir.toFile());
+
+    Map<String, String> options = new HashMap<>();
+    options.put(MysqldResourceI.PORT, Integer.toString(port));
+    options.put("character-set-server", "utf8");
+    options.put("sql-mode", "ONLY_FULL_GROUP_BY,NO_AUTO_VALUE_ON_ZERO,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION");
+    options.put("log-output", "NONE");
+
+    mysqldResource.start("embedded-mysql", options);
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      LOG.info("Shutting down embedded MySQL");
+      mysqldResource.shutdown();
+      try {
+        deleteDirRecursively(baseDir);
+      } catch (IOException e) {
+        LOG.error("Error deleting embedded MySQL directory", e);
+      }
+    }));
+
+    if (!mysqldResource.isRunning()) {
+      throw new RuntimeException("Unable start embedded MySQL");
+    }
+
+    LOG.info("Singleton Embedded MySQL started successfully");
+
+    String jdbcUrlFormat = "jdbc:mysql://127.0.0.1:%d/%s?createDatabaseIfNotExist=true&autoReconnect=true&characterEncoding=UTF8";
+    String jdbcUrl = String.format(jdbcUrlFormat, mysqldResource.getPort(), "blazar");
+    HikariConfig configuration = new HikariConfig();
+    configuration.setJdbcUrl(jdbcUrl);
+    configuration.setDriverClassName("com.mysql.jdbc.Driver");
+    configuration.setUsername("root");
+    configuration.setPassword("");
+
+    return new HikariManagedDataSource(configuration);
+  }
+
+  private static void deleteDirRecursively(Path dir) throws IOException {
+    Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+      }
+    });
+  }
+}
