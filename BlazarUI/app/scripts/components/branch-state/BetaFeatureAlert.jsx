6f4f5e19af2dea3ee5f7f9c84ef1ac4b02e81326
Andy Huang <ahuang@hubspot.com>
2016-10-11 15:39:31 -0400

    Update beta message

--- BlazarUI/app/scripts/components/branch-state/BetaFeatureAlert.jsx
+++ BlazarUI/app/scripts/components/branch-state/BetaFeatureAlert.jsx
@@ -5,7 +5,13 @@ const BetaFeatureAlert = ({onDismiss}) => {
   return (
     <Alert bsStyle="info" onDismiss={onDismiss}>
       <h4>Beta - tell us what you think!</h4>
-      <p>Hey! Your team has been selected as part of the initial rollout of the new Module-Centric builds page. This page will provide you with a more informative view of the current state of a branch and will eventually become the main page for accessing your builds in the future.</p>
+      <p>
+        Your team has been selected as part of the initial rollout of a new
+        module-centric builds page. This page combines information from the
+        branch build history and build details pages to provide you with a
+        more relevant view of the current state of a branch. It will eventually
+        become the main page for accessing your builds.
+      </p>
       <p>We'd love to hear any feedback you have using the link at the bottom of this page.</p>
     </Alert>
   );
