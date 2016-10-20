f611b5cad0b72e248616e028c8589a5b23749a48
Andy Huang <ahuang@hubspot.com>
2016-10-20 16:46:54 -0400

    Move active and inactive modules to tabs

---
+++ BlazarUI/app/scripts/components/shared/Tabs.jsx
@@ -0,0 +1,60 @@
+import React, { Component, PropTypes } from 'react';
+
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import Tab from 'react-bootstrap/lib/Tab';
+
+class Tabs extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.state = {key: props.defaultActiveKey};
+  }
+
+  handleSelect(key) {
+    this.setState({key});
+  }
+
+  renderTab(child) {
+    if (!React.isValidElement(child) || !child.props.title) {
+      return null;
+    }
+
+    const {title, eventKey, disabled} = child.props;
+    return (
+      <NavItem
+        key={eventKey}
+        eventKey={eventKey}
+        disabled={disabled}
+        className="private-tab"
+      >
+        {title}
+        <span className="private-tab__indicator" />
+      </NavItem>
+    );
+  }
+
+  render() {
+    const {id, children} = this.props;
+    return (
+      <Tab.Container activeKey={this.state.key} onSelect={this.handleSelect} id={id}>
+        <div>
+          <Nav bsClass="private-tabs">
+            {children.map(this.renderTab)}
+          </Nav>
+          <Tab.Content animation={false}>
+            {children}
+          </Tab.Content>
+        </div>
+      </Tab.Container>
+    );
+  }
+}
+
+Tabs.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  defaultActiveKey: PropTypes.string.isRequired,
+  children: PropTypes.node
+};
+
+export default Tabs;
