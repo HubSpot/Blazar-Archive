45f7eaea028536572aafd2666e1c2b7e8641c3d5
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-11-30 13:52:38 -0500

    improve malformed files object allow hubspot-discoveries to handle multiple errors for a single file

--- BlazarBase/src/main/java/com/hubspot/blazar/base/MalformedFile.java
+++ BlazarBase/src/main/java/com/hubspot/blazar/base/MalformedFile.java
@@ -2,6 +2,7 @@ package com.hubspot.blazar.base;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Objects;
 
 public class MalformedFile {
   private final int branchId;
@@ -34,5 +35,25 @@ public class MalformedFile {
 
   public String getDetails() {
     return details;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MalformedFile that = (MalformedFile) o;
+    return branchId == that.branchId &&
+        Objects.equal(type, that.type) &&
+        Objects.equal(path, that.path) &&
+        Objects.equal(details, that.details);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(branchId, type, path, details);
   }
 }
