eed34d929cf2fbe40a35dfee65b16a33d127b79b
Andy Huang <ahuang@hubspot.com>
2016-12-06 16:46:33 -0500

    Fix module items not showing loading icon

--- BlazarUI/app/scripts/components/branch-state/ModuleItem.jsx
+++ BlazarUI/app/scripts/components/branch-state/ModuleItem.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
 import $ from 'jquery';
@@ -7,7 +7,7 @@ import Collapse from 'react-bootstrap/lib/Collapse';
 import ModuleItemSummary from './ModuleItemSummary.jsx';
 import ModuleBuildHistory from './module-build-history/ModuleBuildHistory.jsx';
 
-import { shouldExpandModuleBuildHistory } from '../../selectors/moduleBuildHistorySelectors';
+import { isModuleItemSelected, getLastRequestStartTime, hasPreviouslyLoadedData } from '../../selectors/moduleBuildHistorySelectors';
 import { handleModuleItemClick } from '../../redux-actions/branchStateActions';
 
 import { getCurrentBranchBuild, getCurrentModuleBuild } from '../Helpers';
@@ -28,43 +28,89 @@ const scrollIntoViewIfNeeded = (element) => {
   }
 };
 
-const ModuleItem = ({moduleState, isExpanded, onClick}) => {
-  const module = moduleState.get('module');
-  const lastSuccessfulBuildNumber = moduleState.getIn(['lastSuccessfulModuleBuild', 'buildNumber']);
+const DELAY_BEFORE_SHOWING_LOADING_ICON = 400;
 
-  let moduleItem = null;
-  return (
-    <li className="module-item" ref={(listItem) => {moduleItem = listItem;}}>
-      <ModuleItemSummary
-        module={module}
-        currentModuleBuild={getCurrentModuleBuild(moduleState)}
-        currentBranchBuild={getCurrentBranchBuild(moduleState)}
-        isExpanded={isExpanded}
-        onClick={onClick}
-      />
-      <Collapse in={isExpanded} onEntered={() => scrollIntoViewIfNeeded(moduleItem)}>
-        <div className="module-item__details">
-          <ModuleBuildHistory
-            moduleName={module.get('name')}
-            moduleId={module.get('id')}
-            lastSuccessfulBuildNumber={lastSuccessfulBuildNumber}
-          />
-        </div>
-      </Collapse>
-    </li>
-  );
-};
+class ModuleItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isExpanded: props.isSelected};
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {isSelected, hasPreviouslyLoaded, lastRequestStartTime} = nextProps;
+    clearTimeout(this.expandTimeoutId);
+
+    if (!isSelected) {
+      this.setState({isExpanded: false});
+    } else if (hasPreviouslyLoaded || this.state.isExpanded) {
+      this.setState({isExpanded: true});
+    } else {
+      // initial fetch was started but data has not been loaded yet
+      // adding a delay smooths out the expansion of the module item details
+      const timeSinceInitialFetch = Date.now() - lastRequestStartTime;
+      const initialFetchTakingToLong = timeSinceInitialFetch > DELAY_BEFORE_SHOWING_LOADING_ICON;
+
+      if (initialFetchTakingToLong) {
+        this.setState({isExpanded: true});
+      } else {
+        this.setState({isExpanded: false});
+        this.expandTimeoutId = setTimeout(
+          () => this.setState({isExpanded: true}),
+          DELAY_BEFORE_SHOWING_LOADING_ICON - timeSinceInitialFetch
+        );
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.isExpanded && !prevProps.hasPreviouslyLoaded) {
+      scrollIntoViewIfNeeded(this.moduleItem);
+    }
+  }
+
+  render() {
+    const {moduleState, onClick} = this.props;
+    const {isExpanded} = this.state;
+    const module = moduleState.get('module');
+    const lastSuccessfulBuildNumber = moduleState.getIn(['lastSuccessfulModuleBuild', 'buildNumber']);
+
+    return (
+      <li className="module-item" ref={(listItem) => {this.moduleItem = listItem;}}>
+        <ModuleItemSummary
+          module={module}
+          currentModuleBuild={getCurrentModuleBuild(moduleState)}
+          currentBranchBuild={getCurrentBranchBuild(moduleState)}
+          isExpanded={isExpanded}
+          onClick={onClick}
+        />
+        <Collapse in={isExpanded} onEntered={() => scrollIntoViewIfNeeded(this.moduleItem)}>
+          <div className="module-item__details">
+            <ModuleBuildHistory
+              moduleName={module.get('name')}
+              moduleId={module.get('id')}
+              lastSuccessfulBuildNumber={lastSuccessfulBuildNumber}
+            />
+          </div>
+        </Collapse>
+      </li>
+    );
+  }
+}
 
 ModuleItem.propTypes = {
   moduleState: ImmutablePropTypes.map,
-  isExpanded: PropTypes.bool.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  lastRequestStartTime: PropTypes.number,
+  hasPreviouslyLoaded: PropTypes.bool,
   onClick: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => {
   const moduleId = ownProps.moduleState.getIn(['module', 'id']);
   return {
-    isExpanded: shouldExpandModuleBuildHistory(state, {moduleId})
+    isSelected: isModuleItemSelected(state, {moduleId}),
+    lastRequestStartTime: getLastRequestStartTime(state, {moduleId}),
+    hasPreviouslyLoaded: hasPreviouslyLoadedData(state, {moduleId})
   };
 };
 
