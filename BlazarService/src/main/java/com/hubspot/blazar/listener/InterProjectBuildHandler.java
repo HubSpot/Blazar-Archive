0455826397927a77eccdffef21ade5005c3587c6
Jonathan Goodwin <jgoodwin@hubspot.com>
2016-12-08 18:40:54 -0500

    Fix InterProjectBuild bootstrapping

--- BlazarService/src/main/java/com/hubspot/blazar/listener/InterProjectBuildHandler.java
+++ BlazarService/src/main/java/com/hubspot/blazar/listener/InterProjectBuildHandler.java
@@ -1,15 +1,17 @@
 package com.hubspot.blazar.listener;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimaps;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.SetMultimap;
 import com.google.inject.Inject;
 import com.hubspot.blazar.base.BuildOptions;
@@ -49,7 +51,7 @@ public class InterProjectBuildHandler extends AbstractInterProjectBuildVisitor {
                                   RepositoryBuildService repositoryBuildService,
                                   GitHubHelper gitHubHelper,
                                   InterProjectBuildMappingService interProjectBuildMappingService,
-                                  InterProjectBuildService interProjectBuildService){
+                                  InterProjectBuildService interProjectBuildService) {
     this.dependenciesService = dependenciesService;
     this.moduleService = moduleService;
     this.moduleBuildService = moduleBuildService;
@@ -69,7 +71,7 @@ public class InterProjectBuildHandler extends AbstractInterProjectBuildVisitor {
       s.add(moduleService.get(i).get());
     }
     DependencyGraph d = dependenciesService.buildInterProjectDependencyGraph(s);
-    LOG.debug("Built graph for InterProjectBuild {} in {}", build.getId().get(), System.currentTimeMillis()-start);
+    LOG.debug("Built graph for InterProjectBuild {} in {}", build.getId().get(), System.currentTimeMillis() - start);
     if (s.isEmpty()) {
       interProjectBuildService.finish(InterProjectBuild.getFinishedBuild(build, InterProjectBuild.State.SUCCEEDED));
     }
@@ -92,29 +94,14 @@ public class InterProjectBuildHandler extends AbstractInterProjectBuildVisitor {
       return;
     }
 
-    // This is only for Initiating the build tree, every iteration after this is handled in InterProjectModuleBuildVisitor#visitSuccess()
-    SetMultimap<Integer, Integer> launchable = HashMultimap.create();
-    for (int moduleId : build.getModuleIds()) {
-      int branchId = moduleService.getBranchIdFromModuleId(moduleId);
+    Map<Integer, Collection<Integer>> branchToLaunchableModules = getBuildableModulesPerBranch(d, build.getModuleIds());
 
-      if (shouldBuild(branchId, moduleId, d)) {
-        launchable.put(branchId, moduleId);
-      }
-      // check if should launch any children pro-actively
-      for (int child : d.reachableVertices(moduleId)) {
-        if (shouldBuild(branchId, child, d)) {
-          launchable.put(branchId, child);
-        }
-      }
-    }
-
-    for (Map.Entry<Integer, Set<Integer>> entry : Multimaps.asMap(launchable).entrySet()) {
-      Set<Integer> moduleIds = entry.getValue();
+    for (Map.Entry<Integer, Collection<Integer>> entry : branchToLaunchableModules.entrySet()) {
+      Set<Integer> moduleIds = ImmutableSet.copyOf(entry.getValue());
       BuildTrigger buildTrigger = BuildTrigger.forInterProjectBuild(build);
-      BuildOptions buildOptions = new BuildOptions(entry.getValue(), BuildOptions.BuildDownstreams.NONE, false);
+      BuildOptions buildOptions = new BuildOptions(moduleIds, BuildOptions.BuildDownstreams.NONE, false);
       GitInfo gitInfo = branchService.get(entry.getKey()).get();
       long buildId = repositoryBuildService.enqueue(gitInfo, buildTrigger, buildOptions);
-
       for (int moduleId : moduleIds) {
         interProjectBuildMappingService.insert(InterProjectBuildMapping.makeNewMapping(build.getId().get(), gitInfo.getId().get(), Optional.of(buildId), moduleId));
       }
@@ -134,18 +121,49 @@ public class InterProjectBuildHandler extends AbstractInterProjectBuildVisitor {
     }
   }
 
-  private boolean shouldBuild(int branchId, int moduleId, DependencyGraph graph) {
-    Set<Integer> parents = graph.incomingVertices(moduleId);
-    if (parents.size() == 0) {
-      return true;
-    }
-    // check all parents are in the the specified branch
-    for (int parent : parents) {
-      if (branchId != moduleService.getBranchIdFromModuleId(parent)) {
-        return false;
+  /**
+   *
+   * For each part of an InterProjectBuild we launch a branch builds (repository build)
+   * with a specific set of modules that are to be built. When a module finishes we check
+   * if any downstream dependencies can be started, and start those at that time. This
+   * method helps us launch as many modules as we can (per branch) before the feedback loop
+   * of builds causing other builds to launch has been started.
+   *
+   * Because of certain dependency relationships between projects you cannot guarantee that
+   * all modules in the graph in a given branch can be built together. Sometimes you must build
+   * half of the modules in the branch, then build some external dependency, and then build the rest
+   * of the modules in the branch.
+   *
+   * This method figures out which modules we can launch on a per-branch basis at the start of the InterProjectBuild
+   */
+  private Map<Integer, Collection<Integer>> getBuildableModulesPerBranch(DependencyGraph graph, Set<Integer> originallyTriggeredModuleIds) {
+    SetMultimap<Integer, Integer> branchIdToLaunchableModules = HashMultimap.create();
+    // Modules with no upstreams in our graph become the 'root' nodes from which the InterProject build will spread
+    Set<Integer> rootModules = originallyTriggeredModuleIds.stream().filter(moduleId -> graph.incomingVertices(moduleId).isEmpty()).collect(Collectors.toSet());
+
+    for (int rootModule : rootModules) {
+      int branchId = moduleService.getBranchIdFromModuleId(rootModule);
+      // add this to the map because we want to build it
+      branchIdToLaunchableModules.put(branchId, rootModule);
+
+      /*
+       * To be able to start a build of a downstream module of this root module in the next repository build:
+       *  1. It must share a branch with this one
+       *     If it does not then it will be started either by a different root module, or at a later time
+       *
+       *  2. All of the downstream module's dependencies must share a branch with this one
+       *     If they do not then we need to wait for them to be build to build the next module -- so we cannot build it now
+       */
+      Set<Integer> moduleIdsForBranch = moduleService.getByBranch(branchId).stream().map(module -> module.getId().get()).collect(Collectors.toSet());
+      Set<Integer> downstreamModules = graph.reachableVertices(rootModule);  // all downstream nodes in the graph (recursive)
+
+      for (int downstreamModuleId : downstreamModules) {
+        if (moduleIdsForBranch.contains(downstreamModuleId) && // condition 1
+            moduleIdsForBranch.containsAll(graph.getAllUpstreamNodes(downstreamModuleId))) {  // condition 2
+          branchIdToLaunchableModules.put(branchId, downstreamModuleId);
+        }
       }
     }
-    // check that this module is in the specified branch (used by child builds)
-    return branchId == moduleService.getBranchIdFromModuleId(moduleId);
+    return branchIdToLaunchableModules.asMap();
   }
 }
