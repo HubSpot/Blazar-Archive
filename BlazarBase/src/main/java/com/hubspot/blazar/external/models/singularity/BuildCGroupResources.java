b044277e9301bb4badc5347ad518ea0a94ea31c6
Jonathan Goodwin <jgoodwin@hubspot.com>
2017-03-14 16:09:52 -0400

    fix caps

---
+++ BlazarBase/src/main/java/com/hubspot/blazar/external/models/singularity/BuildCGroupResources.java
@@ -0,0 +1,42 @@
+package com.hubspot.blazar.external.models.singularity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BuildCGroupResources {
+
+  private final double cpus;
+  private final double memoryMb;
+
+  private static BuildCGroupResources fromMesosResources(com.hubspot.mesos.Resources resources) {
+    return new BuildCGroupResources(resources.getCpus(), resources.getMemoryMb());
+  }
+
+  /**
+   * Blazar's view of mesos resources, currently we do not care about ports or disk, so we leave those fields at 0
+   * when converting to Mesos Resources  {@link BuildCGroupResources#toMesosResources(BuildCGroupResources) toMesosResources(Resources)}
+   * @param cpus number of cpu
+   * @param memoryMb mb ram for build
+   */
+  @JsonCreator
+  public BuildCGroupResources(@JsonProperty("cpus") double cpus, @JsonProperty("memoryMb") double memoryMb) {
+    this.cpus = cpus;
+    this.memoryMb = memoryMb;
+  }
+
+  public double getCpus() {
+    return cpus;
+  }
+
+  public double getMemoryMb() {
+    return memoryMb;
+  }
+
+  public BuildCGroupResources add(BuildCGroupResources other) {
+    return new BuildCGroupResources(other.getCpus() + cpus, other.getMemoryMb() + memoryMb);
+  }
+
+  public com.hubspot.mesos.Resources toMesosResources() {
+    return new com.hubspot.mesos.Resources(cpus, memoryMb, 0);
+  }
+}
